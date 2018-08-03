696d741d2beb6c81fb5a03341bec030b66ac62e9
吕朝晖 <lvchaohui@uniondrug.cn>
2018-08-03 11:04:17 +0800

    合并

--- src/Modules/UserSdk.php
+++ src/Modules/UserSdk.php
@@ -93,4 +93,15 @@ class UserSdk extends Sdk implements ServiceSdkInterface
     {
         return $this->restful(static::METHOD_POST, "/member/create", $body);
     }
+
+    /**
+     * 验证码登录
+     * @link https://uniondrug.coding.net/p/docs/git/blob/development/sdks/service/user/member/smsLogin.md
+     * @param array $body
+     * @return ClientResponseInterface
+     */
+    public function smsLogin($body)
+    {
+        return $this->restful(static::METHOD_POST, "/member/smsLogin", $body);
+    }
 }
