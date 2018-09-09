dce7ae3601401841ad424aa6a49b600102f693a4
付义兵 <websearch@163.com>
2018-09-09 15:36:32 +0800

    no message

--- src/ServiceSdk.php
+++ src/ServiceSdk.php
@@ -7,21 +7,22 @@ namespace Uniondrug\ServiceSdk;
 
 /**
  * SDK分发调度
- * @property Modules\CommonSdk   $common        公共服务
- * @property Modules\CustomerSdk $customer      客户服务
- * @property Modules\DataSdk     $data          数据服务
- * @property Modules\EquitySdk   $equity        权益服务
- * @property Modules\GoodsSdk    $goods         商品服务
- * @property Modules\MbsSdk      $mbs           消息总线服务
- * @property Modules\MerchantSdk $merchant      商户服务
- * @property Modules\ProductSdk  $product       产品服务
- * @property Modules\ProjectSdk  $project       项目服务
- * @property Modules\ReportSdk   $report        报表服务
- * @property Modules\RuleSdk     $rule          规则中心/规则引擎
- * @property Modules\UserSdk     $user          用户服务
- * @property Modules\MessageSdk  $message       消息中心
- * @property Modules\OrderSdk    $order         订单中心
- * @property Modules\PaymentsSdk $payments      支付中心
+ * @property Modules\CommonSdk   $common         公共服务
+ * @property Modules\CustomerSdk $customer       客户服务
+ * @property Modules\DataSdk     $data           数据服务
+ * @property Modules\EquitySdk   $equity         权益服务
+ * @property Modules\GoodsSdk    $goods          商品服务
+ * @property Modules\MbsSdk      $mbs            消息总线服务
+ * @property Modules\Mbs2Sdk     $mbs2           新版MBS消息服务
+ * @property Modules\MerchantSdk $merchant       商户服务
+ * @property Modules\ProductSdk  $product        产品服务
+ * @property Modules\ProjectSdk  $project        项目服务
+ * @property Modules\ReportSdk   $report         报表服务
+ * @property Modules\RuleSdk     $rule           规则中心/规则引擎
+ * @property Modules\UserSdk     $user           用户服务
+ * @property Modules\MessageSdk  $message        消息中心
+ * @property Modules\OrderSdk    $order          订单中心
+ * @property Modules\PaymentsSdk $payments       支付中心
  * @package Uniondrug\ServiceSdk
  */
 class ServiceSdk
