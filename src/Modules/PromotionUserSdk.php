58a408ee61f874a620e0704056d9cf840d9dceae
guoyan <guoyan@uniondrug.cn>
2018-09-25 17:47:39 +0800

    scheme&bidding

--- src/Modules/PromotionUserSdk.php
+++ src/Modules/PromotionUserSdk.php
@@ -61,4 +61,15 @@ class PromotionUserSdk extends Sdk implements ServiceSdkInterface
         return $this->restful(static::METHOD_POST, "/login/complete", $body);
     }
 
+    /**
+     * 登陆
+     * @link https://uniondrug.coding.net/p/docs/git/blob/development/sdks/service/promotion/user/login/login.md
+     * @param array $body
+     * @return ClientResponseInterface
+     */
+    public function login($body)
+    {
+        return $this->restful(static::METHOD_POST, "/login/login", $body);
+    }
+
 }
