6653508267409a86e50a10981e7aa4b0abbce289
qishengqiang <john1992@qq.com>
2018-10-24 15:37:11 +0800

    添加订单sdk

--- src/Modules/OrderSdk.php
+++ src/Modules/OrderSdk.php
@@ -35,4 +35,59 @@ class OrderSdk extends Sdk implements ServiceSdkInterface
     {
         return $this->restful(static::METHOD_POST, "/order/edit", $body);
     }
+
+    /**
+     * 订单列表
+     * @link https://uniondrug.coding.net/p/docs/git/blob/development/sdks/service/user/member/login.password.md
+     * @param array $body
+     * @return ClientResponseInterface
+     */
+    public function getList($body)
+    {
+        return $this->restful(static::METHOD_GET, "/order", $body);
+    }
+
+    /**
+     * 订单详情
+     * @link https://uniondrug.coding.net/p/docs/git/blob/development/sdks/service/user/member/login.password.md
+     * @param array $body
+     * @return ClientResponseInterface
+     */
+    public function getDetail($body)
+    {
+        return $this->restful(static::METHOD_POST, "/order/detail", $body);
+    }
+
+    /**
+     * 订单撤单
+     * @link https://uniondrug.coding.net/p/docs/git/blob/development/sdks/service/user/member/login.password.md
+     * @param array $body
+     * @return ClientResponseInterface
+     */
+    public function cancel($body)
+    {
+        return $this->restful(static::METHOD_POST, "/order/cancel", $body);
+    }
+
+    /**
+     * 订单回推
+     * @link https://uniondrug.coding.net/p/docs/git/blob/development/sdks/service/user/member/login.password.md
+     * @param array $body
+     * @return ClientResponseInterface
+     */
+    public function push($body)
+    {
+        return $this->restful(static::METHOD_POST, "/order/push", $body);
+    }
+
+    /**
+     * 创建订单
+     * @link https://uniondrug.coding.net/p/docs/git/blob/development/sdks/service/user/member/login.password.md
+     * @param array $body
+     * @return ClientResponseInterface
+     */
+    public function create($body)
+    {
+        return $this->restful(static::METHOD_POST, "/order/create", $body);
+    }
 }
