9c0edb165b298c44ec5b435eca64413e48a5d8b7
zyzy <cathyzhao@uniondrug.cn>
2018-11-01 15:06:19 +0800

    cashier/confirm

--- src/Modules/CashierSdk.php
+++ src/Modules/CashierSdk.php
@@ -13,7 +13,7 @@ use Uniondrug\ServiceSdk\Sdk;
 use Uniondrug\ServiceSdk\ServiceSdkInterface;
 
 /**
- * 订单服务
+ * 收银台服务
  * @package Uniondrug\ServiceSdk\Modules
  */
 class CashierSdk extends Sdk implements ServiceSdkInterface
@@ -39,5 +39,16 @@ class CashierSdk extends Sdk implements ServiceSdkInterface
     public function consume($body)
     {
         return $this->restful(static::METHOD_POST, "/cashier/consume", $body);
+    }
+
+    /**
+     * 确认支付订单成功与否
+     * @link https://uniondrug.coding.net/p/docs/git/blob/development/sdks/service/user/member/login.password.md
+     * @param array $body
+     * @return ClientResponseInterface
+     */
+    public function confirm($body)
+    {
+        return $this->restful(static::METHOD_POST, "/cashier/confirm", $body);
     }
 }
