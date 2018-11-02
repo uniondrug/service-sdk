83bdffeeef65488bdd94c973622ffa6d5cc3299c
qishengqiang <john1992@qq.com>
2018-11-03 04:14:35 +0800

    更新CashierSdk，添加getOrderNos

--- src/Modules/CashierSdk.php
+++ src/Modules/CashierSdk.php
@@ -62,4 +62,15 @@ class CashierSdk extends Sdk implements ServiceSdkInterface
     {
         return $this->restful(static::METHOD_POST, "/cashier/mode", $body);
     }
+
+    /**
+     * 获取收银台分账订单号
+     * @link https://uniondrug.coding.net/p/docs/git/blob/development/sdks/service/user/member/login.password.md
+     * @param array $body
+     * @return ClientResponseInterface
+     */
+    public function getOrderNos($body)
+    {
+        return $this->restful(static::METHOD_POST, "/cashier/getorders", $body);
+    }
 }
