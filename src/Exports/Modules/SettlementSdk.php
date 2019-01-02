15365ea710ee893b2b2b2fba24e8ec920a084427
nj_liyang <liyang@uniondrug.cn>
2019-01-02 16:06:22 +0800

    x2.0 调整 settlementSdk

--- src/Exports/Modules/SettlementSdk.php
+++ src/Exports/Modules/SettlementSdk.php
@@ -380,4 +380,15 @@ class SettlementSdk extends SdkBase
     {
         return $this->restful("POST", "/settlement/updateSettlementStatus", $body);
     }
+
+    /**
+     * 导出指定对账单内商品列表接口
+     * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/SettlementController/exportStatementGoodsAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function exportStatementGoods($body)
+    {
+        return $this->restful("POST", "/settlement/exportStatementGoods", $body);
+    }
 }
