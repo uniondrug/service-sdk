9cf1f17418636cf45cd708e369229f1283872820
tanglingyun <tanglingyun@uniondrug.cn>
2019-01-03 18:39:52 +0800

    对账单顾客信息

--- src/Modules/SettlementSdk.php
+++ src/Modules/SettlementSdk.php
@@ -138,4 +138,15 @@ class SettlementSdk extends Sdk implements ServiceSdkInterface
     {
         return $this->restful("POST", "/settlement/ordersExport", $body);
     }
+
+    /**
+     * 顾客信息
+     * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/BillController/memberPagingAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function memberPaging($body)
+    {
+        return $this->restful("POST", "/bill/memberPaging", $body);
+    }
 }
