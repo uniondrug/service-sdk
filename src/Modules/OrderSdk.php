49c4218c19b9f21b81964b3197f903eea6f85bd0
wengshushun <wss0823@126.com>
2019-01-03 15:06:40 +0800

    增加 支付完成撤单接口

--- src/Modules/OrderSdk.php
+++ src/Modules/OrderSdk.php
@@ -203,6 +203,17 @@ class OrderSdk extends Sdk implements ServiceSdkInterface
         return $this->restful(static::METHOD_POST, "/order/ordernos", $body);
     }
 
+    /**
+     * 已支付完成后进行撤单
+     * @link https://uniondrug.coding.net/p/module.order/git/blob/development/docs/api/OrderController/cancelRefundAction.md
+     * @param $body
+     * @return ClientResponseInterface
+     */
+    public function cancelRefund($body)
+    {
+        return $this->restful(static::METHOD_POST, "/order/cancelrefund", $body);
+    }
+
 
 
 
