72d307bb848756b4d647598ce3d85675d3b6e5e7
tanglingyun <tanglingyun@uniondrug.cn>
2019-01-07 18:22:16 +0800

    导出顾客信息的sql

--- src/Modules/SettlementSdk.php
+++ src/Modules/SettlementSdk.php
@@ -171,4 +171,15 @@ class SettlementSdk extends Sdk implements ServiceSdkInterface
     {
         return $this->restful("POST", "/settlement/exportStatementGoods", $body);
     }
+
+    /**
+     * 顾客信息
+     * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/BillController/ExportMemberPagingSqlAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function exportMemberPagingSql($body)
+    {
+        return $this->restful("POST", "/bill/exportMemberPagingSql", $body);
+    }
 }
