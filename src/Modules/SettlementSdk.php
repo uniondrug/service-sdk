78876ef4ba4e26156603151caf13cb10fc6b1223
luokuanxing <346300265@qq.com>
2019-01-07 11:22:48 +0800

    SettlementSdk add saleGoodsExport

--- src/Modules/SettlementSdk.php
+++ src/Modules/SettlementSdk.php
@@ -292,4 +292,15 @@ class SettlementSdk extends Sdk implements ServiceSdkInterface
     {
         return $this->restful("POST", "/partner/partnerCount", $body);
     }
+
+    /**
+     * 导出销售清单列表
+     * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/BillController/saleGoodsExportAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function saleGoodsExport($body)
+    {
+        return $this->restful("POST", "/bill/saleGoodsExport", $body);
+    }
 }
