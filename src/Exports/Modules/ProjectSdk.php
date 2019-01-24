8aad16e020bfb1e8b23363368bb88341e86fdf2e
zong <zmhfly8@foxmail.com>
2019-01-24 14:57:06 +0800

    新增： 1、修改激活信息 2、获取方式

--- src/Exports/Modules/ProjectSdk.php
+++ src/Exports/Modules/ProjectSdk.php
@@ -8,8 +8,8 @@
  * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
- * @date   2019-01-21
- * @time   Mon, 21 Jan 2019 16:07:52 +0800
+ * @date   2019-01-24
+ * @time   Thu, 24 Jan 2019 14:51:59 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
@@ -186,6 +186,17 @@ class ProjectSdk extends SdkBase
     }
 
     /**
+     * 修改激活信息
+     * @link https://uniondrug.coding.net/p/module.project/git/blob/development/docs/api/GroupActivateController/editAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function editGroupActivate($body)
+    {
+        return $this->restful("POST", "/group/activate/edit", $body);
+    }
+
+    /**
      * 编辑项目
      * @link https://uniondrug.coding.net/p/module.project/git/blob/development/docs/api/ProjectController/editAction.md
      * @param array $body 入参类型
@@ -337,6 +348,17 @@ class ProjectSdk extends SdkBase
     public function getExportTendency($body)
     {
         return $this->restful("POST", "/export/tendency", $body);
+    }
+
+    /**
+     * 获取的方式
+     * @link https://uniondrug.coding.net/p/module.project/git/blob/development/docs/api/GroupActivateController/listsAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function getGroupActivateLists($body)
+    {
+        return $this->restful("POST", "/group/activate/lists", $body);
     }
 
     /**
