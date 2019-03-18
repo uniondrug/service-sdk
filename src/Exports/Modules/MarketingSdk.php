50de5879a0ed7280cc2dc8c17530f7c3e887cc1a
nj_liyang <liyang@uniondrug.cn>
2019-03-18 14:48:16 +0800

    x2.0 修改marketingSdk

--- src/Exports/Modules/MarketingSdk.php
+++ src/Exports/Modules/MarketingSdk.php
@@ -30,6 +30,17 @@ class MarketingSdk extends SdkBase
     protected $serviceName = 'marketing.module';
 
     /**
+     * 批量添加药品
+     * @link https://uniondrug.coding.net/p/module.marketing/git/tree/development/app/docs/api/DrugController/createAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function MarketingDrugCreate($body)
+    {
+        return $this->restful("POST", "/marketing/drug/create", $body);
+    }
+
+    /**
      * 用户参加营销活动下订单接口
      * @link https://uniondrug.coding.net/p/module.marketing/git/tree/development/app/docs/api/OrderController/createAction.md
      * @param array $body 入参类型
@@ -105,82 +116,4 @@ class MarketingSdk extends SdkBase
     {
         return $this->restful("POST", "/marketing/plan/partner/add", $body);
     }
-
-    /**
-     * 批量开启连锁
-     * @link https://uniondrug.coding.net/p/module.marketing/git/tree/development/app/docs/api/TradeInSwitchController/addAction.md
-     * @param array $body 入参类型
-     * @return ResponseInterface
-     */
-    public function TradeInSwitchAdd($body)
-    {
-        return $this->restful("POST", "/trade/in/switch/adds", $body);
-    }
-
-    /**
-     * 批量开启门店
-     * @link https://uniondrug.coding.net/p/module.marketing/git/tree/development/app/docs/api/TradeInSwitchController/addStoresAction.md
-     * @param array $body 入参类型
-     * @return ResponseInterface
-     */
-    public function TradeInSwitchAddStores($body)
-    {
-        return $this->restful("POST", "/trade/in/switch/add/stores", $body);
-    }
-
-    /**
-     * 批量关闭连锁
-     * @link https://uniondrug.coding.net/p/module.marketing/git/tree/development/app/docs/api/TradeInSwitchController/delAction.md
-     * @param array $body 入参类型
-     * @return ResponseInterface
-     */
-    public function TradeInSwitchDel($body)
-    {
-        return $this->restful("POST", "/trade/in/switch/del", $body);
-    }
-
-    /**
-     * 批量关闭门店
-     * @link https://uniondrug.coding.net/p/module.marketing/git/tree/development/app/docs/api/TradeInSwitchController/delStoresAction.md
-     * @param array $body 入参类型
-     * @return ResponseInterface
-     */
-    public function TradeInSwitchDelStores($body)
-    {
-        return $this->restful("POST", "/trade/in/switch/del/stores", $body);
-    }
-
-    /**
-     * 查看信息
-     * @link https://uniondrug.coding.net/p/module.marketing/git/tree/development/app/docs/api/TradeInSwitchController/getInfoAction.md
-     * @param array $body 入参类型
-     * @return ResponseInterface
-     */
-    public function TradeInSwitchGetInfo($body)
-    {
-        return $this->restful("POST", "/trade/in/switch/get/info", $body);
-    }
-
-    /**
-     * 连锁及门店列表
-     * @link https://uniondrug.coding.net/p/module.marketing/git/tree/development/app/docs/api/TradeInSwitchController/listAction.md
-     * @param array $body 入参类型
-     * @return ResponseInterface
-     */
-    public function TradeInSwitchList($body)
-    {
-        return $this->restful("POST", "/trade/in/switch/list", $body);
-    }
-
-    /**
-     * 连锁列表
-     * @link https://uniondrug.coding.net/p/module.marketing/git/tree/development/app/docs/api/TradeInSwitchController/partnerListAction.md
-     * @param array $body 入参类型
-     * @return ResponseInterface
-     */
-    public function TradeInSwitchPartnerList($body)
-    {
-        return $this->restful("POST", "/trade/in/switch/partner/list", $body);
-    }
-
 }
