d69bff28e6b8a8d02eecc8b42a197ca8d4101612
william <william_wqq@163.com>
2019-04-22 16:45:16 +0800

    修改命名空间

--- src/Exports/Modules/DataSdk.php
+++ src/Exports/Modules/DataSdk.php
@@ -1,9 +1,19 @@
 <?php
+<?
+php
 /**
- * @author htr
- * @date   2018-04-23
+ * 重要说明
+ * 1. 本文件由Postman命令脚本自动生成, 请不要修改, 若需修改
+ *    请通过`php console postman`命令重新生成.
+ * 2. 本脚本在生成时, 依赖所在项目的Controller有 `@Sdk method`定义,
+ *    同时, 项目根目录下的`postman.json`需有`sdk`、`sdkLink`定义
+ * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
+ *    的`src/Exports/Modules`目录下，并发重新发布release版本.
+ * @author PostmanCommand
+ * @date   2018-12-25
+ * @time   Tue, 25 Dec 2018 15:48:21 +0800
  */
-namespace Uniondrug\ServiceSdk\Modules;
+namespace Uniondrug\ServiceSdk\Exports\Modules;
 
 use Uniondrug\ServiceSdk\Exports\Abstracts\SdkBase;
 use Uniondrug\ServiceSdk\Responses\ResponseInterface;
