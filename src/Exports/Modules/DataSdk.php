f0cf79dffd2896a51d6255a2552a727a6cc9de9c
william <william_wqq@163.com>
2019-04-22 16:42:15 +0800

    优化DataSdk

--- src/Exports/Modules/DataSdk.php
+++ src/Exports/Modules/DataSdk.php
@@ -5,70 +5,69 @@
  */
 namespace Uniondrug\ServiceSdk\Modules;
 
-use Uniondrug\ServiceSdk\Sdk;
-use Uniondrug\ServiceSdk\ServiceSdkInterface;
-use Uniondrug\Service\ClientResponseInterface;
+use Uniondrug\ServiceSdk\Exports\Abstracts\SdkBase;
+use Uniondrug\ServiceSdk\Responses\ResponseInterface;
 
 /**
- * 数据
+ * Class DataSdk
  * @package Uniondrug\ServiceSdk\Modules
  */
-class DataSdk extends Sdk implements ServiceSdkInterface
+class DataSdk extends SdkBase
 {
-    protected $serviceName = 'data';
+    protected $serviceName = 'data.module';
 
     /**
      * 地址区域tree
      * @link https://uniondrug.coding.net/p/docs/git/blob/development/sdks/service/data/areas/getTree.md
      * @param array $body
-     * @return ClientResponseInterface
+     * @return ResponseInterface
      */
     public function getTree($body)
     {
-        return $this->restful(static::METHOD_POST, "/areas/tree", $body);
+        return $this->restful("POST", "/areas/tree", $body);
     }
 
     /**
      * 用code码获取列表
      * @link https://uniondrug.coding.net/p/docs/git/blob/development/sdks/service/data/areas/getDetail.md
      * @param array $body
-     * @return ClientResponseInterface
+     * @return ResponseInterface
      */
     public function getDetail($body)
     {
-        return $this->restful(static::METHOD_POST, "/areas/detail", $body);
+        return $this->restful("POST", "/areas/detail", $body);
     }
 
     /**
      * 发送验证码
      * @link https://uniondrug.coding.net/p/docs/git/blob/development/sdks/service/data/sms/sendCaptcha.md
      * @param array $body
-     * @return ClientResponseInterface
+     * @return ResponseInterface
      */
     public function sendCaptcha($body)
     {
-        return $this->restful(static::METHOD_POST, "/sms/sendCaptcha", $body);
+        return $this->restful("POST", "/sms/sendCaptcha", $body);
     }
 
     /**
      * 对验证码进行验证
      * @link https://uniondrug.coding.net/p/docs/git/blob/development/sdks/service/data/sms/validateCaptcha.md
      * @param array $body
-     * @return ClientResponseInterface
+     * @return ResponseInterface
      */
     public function validateCaptcha($body)
     {
-        return $this->restful(static::METHOD_POST, "/sms/validateCaptcha", $body);
+        return $this->restful("POST", "/sms/validateCaptcha", $body);
     }
 
     /**
      * 地区模糊模询
      * @link https://uniondrug.coding.net/p/docs/git/blob/development/sdks/service/data/sms/validateCaptcha.md
      * @param array $body
-     * @return ClientResponseInterface
+     * @return ResponseInterface
      */
     public function areasSearch($body)
     {
-        return $this->restful(static::METHOD_POST, "/areas/search", $body);
+        return $this->restful("POST", "/areas/search", $body);
     }
 }
