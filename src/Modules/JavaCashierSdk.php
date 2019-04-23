b24b7fb7ae12cbbcc5c437204973ea7876ae68ef
吕朝晖 <lvchaohui@uniondrug.cn>
2019-04-23 15:37:54 +0800

    java收银

---
+++ src/Modules/JavaCashierSdk.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lvchaohui
+ * Date: 2019/4/23
+ * Time: 3:33 PM
+ */
+namespace Uniondrug\ServiceSdk\Modules;
+
+use Uniondrug\Service\ClientResponseInterface;
+use Uniondrug\ServiceSdk\Sdk;
+use Uniondrug\ServiceSdk\ServiceSdkInterface;
+use Uniondrug\Structs\StructInterface;
+
+/**
+ * JavaCashierSdk
+ * @package Uniondrug\ServiceSdk\Modules
+ */
+class JavaCashierSdk extends Sdk implements ServiceSdkInterface
+{
+    /**
+     * 服务名称
+     * @var string
+     */
+    protected $serviceName = 'javaCashier';
+
+    /**
+     * 订单退款
+     * @link
+     * @param array $body 入参类型
+     * @return ClientResponseInterface
+     */
+    public function orderCashierRefund($body)
+    {
+        return $this->restful("POST", "/cashier/refund", $body);
+    }
+
+    /**
+     * 订单支付
+     * @link
+     * @param array $body 入参类型
+     * @return ClientResponseInterface
+     */
+    public function orderCashierCreate($body)
+    {
+        return $this->restful("POST", "/cashier/create", $body);
+    }
+}
