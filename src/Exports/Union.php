c1c2ce5a1521f3f9828cd687f956356cc508e494
zong <zmhfly8@foxmail.com>
2019-04-28 15:45:04 +0800

    添加 union.project ide 提示

--- src/Exports/Union.php
+++ src/Exports/Union.php
@@ -6,10 +6,12 @@
 namespace Uniondrug\ServiceSdk\Exports;
 
 use Uniondrug\ServiceSdk\Exports\Unions\FinanceSdk;
+use Uniondrug\ServiceSdk\Exports\Unions\ProjectSdk;
 
 /**
  * 聚合类SDK入口
  * @property FinanceSdk $finance
+ * @property ProjectSdk $project
  * @package Uniondrug\ServiceSdk\Exports
  */
 class Union extends Abstracts\SdkEntrypoint
