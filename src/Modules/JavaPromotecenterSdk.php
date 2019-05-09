9fa403522168ce7afcb0f0b00aeabb8d09140f0f
tangly <tanglingyun@uniondrug.cn>
2019-05-09 11:36:55 +0800

    促销中心营销方案javaSdk

---
+++ src/Modules/JavaPromotecenterSdk.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhupengfei
+ * Date: 2019-05-09
+ * Time: 11:08
+ */
+namespace Uniondrug\ServiceSdk\Exports\Backends;
+
+use Uniondrug\Service\ClientResponseInterface;
+use Uniondrug\ServiceSdk\Sdk;
+use Uniondrug\ServiceSdk\ServiceSdkInterface;
+use Uniondrug\Structs\StructInterface;
+
+class JavaPromotecenterSdk extends Sdk implements ServiceSdkInterface
+{
+    /**
+     * 服务名称
+     * 自来`postman.json`文件定义的`sdkService`值
+     * @var string
+     */
+    protected $serviceName = 'java.middleend.promotecenter';
+
+    /**
+     * 创建方案(活动方案)
+     * @link https://uniondrug.coding.net/p/java.middleend.promotecenter/git/blob/development/doc/%E4%BF%83%E9%94%80%E4%B8%AD%E5%BF%83%E6%8E%A5%E5%8F%A3%E6%96%87%E6%A1%A3(%E6%96%B9%E6%A1%88%E7%9B%B8%E5%85%B3).md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function addScheme($body)
+    {
+        return $this->restful("POST", "/promote/scheme/add", $body);
+    }
+
+    /**
+     * 方案详情展示(活动方案)
+     * @link https://uniondrug.coding.net/p/java.middleend.promotecenter/git/blob/development/doc/%E4%BF%83%E9%94%80%E4%B8%AD%E5%BF%83%E6%8E%A5%E5%8F%A3%E6%96%87%E6%A1%A3(%E6%96%B9%E6%A1%88%E7%9B%B8%E5%85%B3).md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function detailScheme($body)
+    {
+        return $this->restful("POST", "/promote/scheme/info", $body);
+    }
+
+    /**
+     * 方案列表展示(活动方案)
+     * @link https://uniondrug.coding.net/p/java.middleend.promotecenter/git/blob/development/doc/%E4%BF%83%E9%94%80%E4%B8%AD%E5%BF%83%E6%8E%A5%E5%8F%A3%E6%96%87%E6%A1%A3(%E6%96%B9%E6%A1%88%E7%9B%B8%E5%85%B3).md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function pagingScheme($body)
+    {
+        return $this->restful("POST", "/promote/scheme/paging", $body);
+    }
+
+    /**
+     * 方案停用(活动方案)
+     * @link https://uniondrug.coding.net/p/java.middleend.promotecenter/git/blob/development/doc/%E4%BF%83%E9%94%80%E4%B8%AD%E5%BF%83%E6%8E%A5%E5%8F%A3%E6%96%87%E6%A1%A3(%E6%96%B9%E6%A1%88%E7%9B%B8%E5%85%B3).md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function stopScheme($body)
+    {
+        return $this->restful("POST", "/promoteCenter/scheme/stop", $body);
+    }
+
+    /**
+     * 合作渠道修改(活动方案)
+     * @link https://uniondrug.coding.net/p/java.middleend.promotecenter/git/blob/development/doc/%E4%BF%83%E9%94%80%E4%B8%AD%E5%BF%83%E6%8E%A5%E5%8F%A3%E6%96%87%E6%A1%A3(%E6%96%B9%E6%A1%88%E7%9B%B8%E5%85%B3).md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function modifyPartner($body)
+    {
+        return $this->restful("POST", "/promote/scheme/modifyPartner", $body);
+    }
+}
