369fdb08c8d89185fedd63f74a5a7490bdc01b08
luokuanxing <346300265@qq.com>
2019-05-22 09:40:23 +0800

    SettlementSdk 添加开始理赔结算单的接口

--- src/Modules/SettlementSdk.php
+++ src/Modules/SettlementSdk.php
@@ -1590,5 +1590,17 @@ class SettlementSdk extends Sdk implements ServiceSdkInterface
     {
         return $this->restful("POST", "/statements/summary", $body);
     }
+
+    /**
+     * 开始理赔结算单
+     * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/StatementsController/summaryAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function statementStartClaim($body)
+    {
+        return $this->restful("POST", "/statements/startClaim", $body);
+    }
+
 }
 
