f9f4da3530311542b2905a70700f17386483b57e
刘超 <liuchao@uniondrug.cn>
2019-05-22 11:27:06 +0800

    wxapi

---
+++ src/Modules/WxapiSdk.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * @author wsfuyibing <websearch@163.com>
+ * @date   2018-03-27
+ */
+namespace Uniondrug\ServiceSdk\Modules;
+
+use Uniondrug\Service\ClientResponseInterface;
+use Uniondrug\ServiceSdk\Sdk;
+use Uniondrug\ServiceSdk\ServiceSdkInterface;
+
+/**
+ * 公共服务
+ * @package Uniondrug\ServiceSdk\Modules
+ */
+class WxapiSdk extends Sdk implements ServiceSdkInterface
+{
+    protected $serviceName = 'wxapi';
+
+    /**
+     * 无门槛代理人绑定
+     * @link https://uniondrug.coding.net/p/module.health/git/blob/development/docs/api/WxController/tokenAction.md
+     * @param array $body
+     * @return ClientResponseInterface
+     */
+    public function bdingAgent($body)
+    {
+        return $this->restful(static::METHOD_POST, '/distribution/dst/share/bdingAgent', $body);
+    }
+
+    /**
+     * 获取用户信息
+     * @link https://uniondrug.coding.net/p/module.health/git/blob/development/docs/api/WxController/tokenAction.md
+     * @param array $body
+     * @return ClientResponseInterface
+     */
+    public function queryMemberInfo($body)
+    {
+        return $this->restful(static::METHOD_POST, '/distribution/dst/share/queryMemberInfo', $body);
+    }
+
+    /**
+     * 获取用户积分
+     * @link https://uniondrug.coding.net/p/module.health/git/blob/development/docs/api/WxController/tokenAction.md
+     * @param array $body
+     * @return ClientResponseInterface
+     */
+    public function getAgentIntegralInfo($body)
+    {
+        return $this->restful(static::METHOD_POST, '/distribution/dst/api/getAgentIntegralInfo', $body);
+    }
+
+    /**
+     * 分销产品分佣比例
+     * @link https://uniondrug.coding.net/p/module.health/git/blob/development/docs/api/WxController/tokenAction.md
+     * @param array $body
+     * @return ClientResponseInterface
+     */
+    public function queryGoodsByChannelId($body)
+    {
+        return $this->restful(static::METHOD_POST, '/distribution/dst/api/queryGoodsByChannelId', $body);
+    }
+
+    /**
+     * 获取渠道下最低消费
+     * @link https://uniondrug.coding.net/p/module.health/git/blob/development/docs/api/WxController/tokenAction.md
+     * @param array $body
+     * @return ClientResponseInterface
+     */
+    public function getMinOrderPriceByChannelId($body)
+    {
+        return $this->restful(static::METHOD_POST, '/distribution/dst/api/getMinOrderPriceByChannelId', $body);
+    }
+
+
+}
