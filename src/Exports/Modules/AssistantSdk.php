fd4ecd1a7cf59e7594991085f1c1819340c4d7cb
朱鹏飞 <zhupengfei@uniondrug.cn>
2019-06-10 10:49:19 +0800

    助手模块 sdk

---
+++ src/Exports/Modules/AssistantSdk.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * 重要说明
+ * 1. 本文件由Postman命令脚本自动生成, 请不要修改, 若需修改
+ *    请通过`php console postman`命令重新生成.
+ * 2. 本脚本在生成时, 依赖所在项目的Controller有 `@Sdk method`定义,
+ *    同时, 项目根目录下的`postman.json`需有`sdk`、`sdkLink`定义
+ * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
+ *    的`src/Exports/Modules`目录下，并发重新发布release版本.
+ * @author PostmanCommand
+ * @date   2019-06-06
+ * @time   Thu, 06 Jun 2019 18:36:02 +0800
+ */
+namespace Uniondrug\ServiceSdk\Exports\Modules;
+
+use Uniondrug\ServiceSdk\Exports\Abstracts\SdkBase;
+use Uniondrug\ServiceSdk\Responses\ResponseInterface;
+
+/**
+ * AssistantSdk
+ * @package Uniondrug\ServiceSdk\Modules
+ */
+class AssistantSdk extends SdkBase
+{
+    /**
+     * 服务名称
+     * 自来`postman.json`文件定义的`sdkService`值
+     * @var string
+     */
+    protected $serviceName = 'assistant.module';
+
+    /**
+     * 邀请函绑定
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Invite/InviteController/BindAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function inviteBind($body)
+    {
+        return $this->restful("POST", "/invite/bind", $body);
+    }
+}
