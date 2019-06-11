79598c149cef6eb7c4222d2e2b65aca8dc5a68a0
吕朝晖 <lvchaohui@uniondrug.cn>
2019-06-11 20:32:25 +0800

    1

--- src/Modules/MarketingSdk.php
+++ src/Modules/MarketingSdk.php
@@ -239,4 +239,15 @@ class MarketingSdk extends Sdk implements ServiceSdkInterface
     {
         return $this->restful("POST", "/marketing/order/orderCount", $body);
     }
+
+    /**
+     * userAction()
+     * @link https://uniondrug.coding.net/p/module.marketing/git/blob/development/docs/api/WhiteController/userAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function evidenceUse($body)
+    {
+        return $this->restful("POST", "/evidence/use", $body);
+    }
 }
