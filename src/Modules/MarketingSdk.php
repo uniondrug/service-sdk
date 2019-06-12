63cbc986145a51ed6f9bb39ac8e4f1b416dc3508
吕朝晖 <lvchaohui@uniondrug.cn>
2019-06-12 17:34:38 +0800

    1

--- src/Modules/MarketingSdk.php
+++ src/Modules/MarketingSdk.php
@@ -261,4 +261,26 @@ class MarketingSdk extends Sdk implements ServiceSdkInterface
     {
         return $this->restful("POST", "/evidence/update", $body);
     }
+
+    /**
+     * userAction()
+     * @link https://uniondrug.coding.net/p/module.marketing/git/blob/development/docs/api/WhiteController/userAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function marketingPlanList($body)
+    {
+        return $this->restful("POST", "/marketing/plan/list", $body);
+    }
+
+    /**
+     * userAction()
+     * @link https://uniondrug.coding.net/p/module.marketing/git/blob/development/docs/api/WhiteController/userAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function marketingPlanPartnerList($body)
+    {
+        return $this->restful("POST", "/marketing/plan/partner/list", $body);
+    }
 }
