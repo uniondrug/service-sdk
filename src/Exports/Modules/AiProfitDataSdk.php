5a54a68fa96d06846f5314fe0593c8dfd1d7164e
william <william_wqq@163.com>
2019-06-18 11:36:10 +0800

    AiProfitDataSdk新增取消banner接口

--- src/Exports/Modules/AiProfitDataSdk.php
+++ src/Exports/Modules/AiProfitDataSdk.php
@@ -96,6 +96,17 @@ class AiProfitDataSdk extends SdkBase
     }
 
     /**
+     * 取消首页Banner
+     * @link https://uniondrug.coding.net/p/module.data.aiProfit/git/blob/development/docs/api/ActivityController/cancelBannerAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function cancelActivityBanner($body)
+    {
+        return $this->restful("POST", "/activity/banner/cancel", $body);
+    }
+
+    /**
      * 下架
      * @link https://uniondrug.coding.net/p/module.data.aiProfit/git/blob/development/docs/api/ActivityController/disableAction.md
      * @param array $body 入参类型
