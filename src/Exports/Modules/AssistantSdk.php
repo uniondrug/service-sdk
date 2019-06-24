f3e2ce7eabfd723214980a7f6b345658b56e142b
xingshenqiang@uniondrug.cn <xingshenqiang@uniondrug.cn>
2019-06-24 11:05:55 +0800

    更新AssistantSdk

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -8,8 +8,8 @@
  * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
- * @date   2019-06-18
- * @time   Tue, 18 Jun 2019 12:26:57 +0800
+ * @date   2019-06-24
+ * @time   Mon, 24 Jun 2019 11:02:00 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
@@ -85,6 +85,83 @@ class AssistantSdk extends SdkBase
     }
 
     /**
+     * 全部列表
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/OperationTabsController/listingAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function operationTabsListing($body)
+    {
+        return $this->restful("POST", "/operationTabs/listing", $body);
+    }
+
+    /**
+     * 新增角色
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/RoleController/createAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function roleCreate($body)
+    {
+        return $this->restful("POST", "/role/create", $body);
+    }
+
+    /**
+     * 全部角色列表
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/RoleController/listingAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function roleListing($body)
+    {
+        return $this->restful("POST", "/role/listing", $body);
+    }
+
+    /**
+     * 获取角色权限列表
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/RoleController/listingAuthAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function roleListingAuth($body)
+    {
+        return $this->restful("POST", "/role/listing/auth", $body);
+    }
+
+    /**
+     * 分页列表
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/RoleController/pagingAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function rolePaging($body)
+    {
+        return $this->restful("POST", "/role/paging", $body);
+    }
+
+    /**
+     * 分配权限
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/RoleController/settingAuthAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function roleSettingAuth($body)
+    {
+        return $this->restful("POST", "/role/setting/auth", $body);
+    }
+
+    /**
+     * 修改
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/RoleController/updateAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function roleUpdate($body)
+    {
+        return $this->restful("POST", "/role/update", $body);
+    }
+
+    /**
      * 获取一条助手用户新
      * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/DataService/UserController/infoAction.md
      * @param array $body 入参类型
@@ -104,5 +181,27 @@ class AssistantSdk extends SdkBase
     public function userInvites($body)
     {
         return $this->restful("POST", "/invite/userinvites", $body);
+    }
+
+    /**
+     * 详情
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/UsersController/detailAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function usersDetail($body)
+    {
+        return $this->restful("POST", "/users/detail", $body);
+    }
+
+    /**
+     * 分页列表
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/UsersController/pagingAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function usersPaging($body)
+    {
+        return $this->restful("POST", "/users/paging", $body);
     }
 }
