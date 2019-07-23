e4cfe419b0101291183ce8120ad76669888d509f
朱鹏飞 <zhupengfei@uniondrug.cn>
2019-07-23 17:52:34 +0800

    退款审核单号和客服业务数据模块相关接口

---
+++ src/Exports/Modules/CustomerserviceSdk.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * 重要说明
+ * 1. 本文件由Postman命令脚本自动生成, 请不要修改, 若需修改
+ *    请通过`php console postman`命令重新生成.
+ * 2. 本脚本在生成时, 依赖所在项目的Controller有 `@Sdk method`定义,
+ *    同时, 项目根目录下的`postman.json`需有`sdk`、`sdkLink`定义
+ * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
+ *    的`src/Exports/Modules`目录下，并发重新发布release版本.
+ * @author PostmanCommand
+ * @date   2019-07-23
+ * @time   Tue, 23 Jul 2019 17:11:20 +0800
+ */
+namespace Uniondrug\ServiceSdk\Exports\Modules;
+
+use Uniondrug\ServiceSdk\Exports\Abstracts\SdkBase;
+use Uniondrug\ServiceSdk\Responses\ResponseInterface;
+
+/**
+ * CustomerserviceSdk
+ * @package Uniondrug\ServiceSdk\Modules
+ */
+class CustomerserviceSdk extends SdkBase
+{
+    /**
+     * 服务名称
+     * 自来`postman.json`文件定义的`sdkService`值
+     * @var string
+     */
+    protected $serviceName = 'customerservice.module';
+
+    /**
+     * 退款申请记录添加
+     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/Refund/ApplyController/createAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function orderRefundApplyCreate($body)
+    {
+        return $this->restful("POST", "/refundapply/create", $body);
+    }
+}
