400f9fe30f81bb38aeb097e9fef47cd75b3d15fd
zong <zmhfly8@foxmail.com>
2019-08-05 09:18:59 +0800

    java 相关接口

---
+++ src/Exports/Modules/JavaFinSdk.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @name   JavaOrderSdk
+ * @date   2018-12-04
+ * @time   Tue, 04 Dec 2018 18:29:57 +0800
+ */
+namespace Uniondrug\ServiceSdk\Exports\Modules;
+
+use Uniondrug\ServiceSdk\Exports\Abstracts\SdkBase;
+use Uniondrug\ServiceSdk\Responses\ResponseInterface;
+
+/**
+ * JavaOrderSdk
+ * @package Uniondrug\ServiceSdk\Modules
+ */
+class JavaOrderCodeSdk extends SdkBase
+{
+    /**
+     * 服务名称
+     * @var string
+     */
+    protected $serviceName = 'java.ordercode';
+
+    /**
+     * 创建审核单单号
+     * @link https://uniondrug.coding.net/p/java.module.order/git/blob/development/uniondrug/uniondrug-services/uniondrug-ordercode-service/CHANGELOG.MD#user-content-432
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function createAuditNo($body)
+    {
+        return $this->restful("POST", "/orderno/audit/creat", $body);
+    }
+
+}
