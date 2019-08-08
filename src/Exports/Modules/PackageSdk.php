2f861e82586e541c8506ba6a0f2d2b1f9ae66102
xingshenqiang@uniondrug.cn <xingshenqiang@uniondrug.cn>
2019-08-08 18:03:05 +0800

    添加packageSdk

--- src/Exports/Modules/PackageSdk.php
+++ src/Exports/Modules/PackageSdk.php
@@ -9,7 +9,7 @@
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
  * @date   2019-08-08
- * @time   Thu, 08 Aug 2019 15:25:18 +0800
+ * @time   Thu, 08 Aug 2019 18:02:08 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
@@ -28,6 +28,226 @@ class PackageSdk extends SdkBase
      * @var string
      */
     protected $serviceName = 'package.module';
+
+    /**
+     * 新增
+     * @link https://uniondrug.coding.net/p/module.package/git/blob/development/docs/api/GuaranteesController/createAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function guaranteesCreate($body)
+    {
+        return $this->restful("POST", "/guarantees/create", $body);
+    }
+
+    /**
+     * 删除
+     * @link https://uniondrug.coding.net/p/module.package/git/blob/development/docs/api/GuaranteesController/deleteAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function guaranteesDelete($body)
+    {
+        return $this->restful("POST", "/guarantees/delete", $body);
+    }
+
+    /**
+     * 详情
+     * @link https://uniondrug.coding.net/p/module.package/git/blob/development/docs/api/GuaranteesController/detailAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function guaranteesDetail($body)
+    {
+        return $this->restful("POST", "/guarantees/detail", $body);
+    }
+
+    /**
+     * 无分页列表
+     * @link https://uniondrug.coding.net/p/module.package/git/blob/development/docs/api/GuaranteesController/listingAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function guaranteesListing($body)
+    {
+        return $this->restful("POST", "/guarantees/listing", $body);
+    }
+
+    /**
+     * 分页列表
+     * @link https://uniondrug.coding.net/p/module.package/git/blob/development/docs/api/GuaranteesController/pagingAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function guaranteesPaging($body)
+    {
+        return $this->restful("POST", "/guarantees/paging", $body);
+    }
+
+    /**
+     * 修改
+     * @link https://uniondrug.coding.net/p/module.package/git/blob/development/docs/api/GuaranteesController/updateAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function guaranteesUpdate($body)
+    {
+        return $this->restful("POST", "/guarantees/update", $body);
+    }
+
+    /**
+     * 新增
+     * @link https://uniondrug.coding.net/p/module.package/git/blob/development/docs/api/LogsController/createAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function logsCreate($body)
+    {
+        return $this->restful("POST", "/logs/create", $body);
+    }
+
+    /**
+     * 删除
+     * @link https://uniondrug.coding.net/p/module.package/git/blob/development/docs/api/LogsController/deleteAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function logsDelete($body)
+    {
+        return $this->restful("POST", "/logs/delete", $body);
+    }
+
+    /**
+     * 详情
+     * @link https://uniondrug.coding.net/p/module.package/git/blob/development/docs/api/LogsController/detailAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function logsDetail($body)
+    {
+        return $this->restful("POST", "/logs/detail", $body);
+    }
+
+    /**
+     * 无分页列表
+     * @link https://uniondrug.coding.net/p/module.package/git/blob/development/docs/api/LogsController/listingAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function logsListing($body)
+    {
+        return $this->restful("POST", "/logs/listing", $body);
+    }
+
+    /**
+     * 分页列表
+     * @link https://uniondrug.coding.net/p/module.package/git/blob/development/docs/api/LogsController/pagingAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function logsPaging($body)
+    {
+        return $this->restful("POST", "/logs/paging", $body);
+    }
+
+    /**
+     * 修改
+     * @link https://uniondrug.coding.net/p/module.package/git/blob/development/docs/api/LogsController/updateAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function logsUpdate($body)
+    {
+        return $this->restful("POST", "/logs/update", $body);
+    }
+
+    /**
+     * 新增产品（换药）
+     * @link https://uniondrug.coding.net/p/module.package/git/blob/development/docs/api/PackageController/createAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function packageCreate($body)
+    {
+        return $this->restful("POST", "/package/create", $body);
+    }
+
+    /**
+     * 新增产品（非换药）
+     * @link https://uniondrug.coding.net/p/module.package/git/blob/development/docs/api/PackageController/createNoAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function packageCreateNo($body)
+    {
+        return $this->restful("POST", "/package/createNo", $body);
+    }
+
+    /**
+     * 详情
+     * @link https://uniondrug.coding.net/p/module.package/git/blob/development/docs/api/PackageController/detailAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function packageDetail($body)
+    {
+        return $this->restful("POST", "/package/detail", $body);
+    }
+
+    /**
+     * 产品状态关闭
+     * @link https://uniondrug.coding.net/p/module.package/git/blob/development/docs/api/PackageController/disableAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function packageDisable($body)
+    {
+        return $this->restful("POST", "/package/disable", $body);
+    }
+
+    /**
+     * 产品状态开启
+     * @link https://uniondrug.coding.net/p/module.package/git/blob/development/docs/api/PackageController/enableAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function packageEnable($body)
+    {
+        return $this->restful("POST", "/package/enable", $body);
+    }
+
+    /**
+     * 无分页列表
+     * @link https://uniondrug.coding.net/p/module.package/git/blob/development/docs/api/PackageController/listingAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function packageListing($body)
+    {
+        return $this->restful("POST", "/package/listing", $body);
+    }
+
+    /**
+     * 分页列表
+     * @link https://uniondrug.coding.net/p/module.package/git/blob/development/docs/api/PackageController/pagingAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function packagePaging($body)
+    {
+        return $this->restful("POST", "/package/paging", $body);
+    }
+
+    /**
+     * 修改
+     * @link https://uniondrug.coding.net/p/module.package/git/blob/development/docs/api/PackageController/updateAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function packageUpdate($body)
+    {
+        return $this->restful("POST", "/package/update", $body);
+    }
 
     /**
      * 新增
