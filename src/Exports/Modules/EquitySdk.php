41e1f701a4416ec3a19bfe5d6722bb6d69e02265
qinguofeng <qinguofeng@uniondrug.cn>
2019-09-18 13:54:34 +0800

    equitySDK

--- src/Exports/Modules/EquitySdk.php
+++ src/Exports/Modules/EquitySdk.php
@@ -9,7 +9,7 @@
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
  * @date   2019-09-18
- * @time   Wed, 18 Sep 2019 11:02:42 +0800
+ * @time   Wed, 18 Sep 2019 13:47:32 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
@@ -359,13 +359,13 @@ class EquitySdk extends SdkBase
 
     /**
      * 权益账号余额多条查询
-     * @link https://uniondrug.coding.net/p/module.equity/git/tree/development/docs/api/EquityExtendController/detailsAction.md
+     * @link https://uniondrug.coding.net/p/module.equity/git/tree/development/docs/api/EquityExtendController/detailAllAction.md
      * @param array $body 入参类型
      * @return ResponseInterface
      */
     public function extendDetailAll($body)
     {
-        return $this->restful("POST", "/equity/extend/details", $body);
+        return $this->restful("POST", "/equity/extend/detailAll", $body);
     }
 
     /**
