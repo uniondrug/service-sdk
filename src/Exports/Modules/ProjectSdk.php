6e9592d5642831e89ab2a49cd4aea3806676506e
zong <zmhfly8@foxmail.com>
2019-10-14 17:36:33 +0800

    module.project 新增分组领取限制

--- src/Exports/Modules/ProjectSdk.php
+++ src/Exports/Modules/ProjectSdk.php
@@ -1021,4 +1021,15 @@ class ProjectSdk extends SdkBase
     {
         return $this->restful("POST", "/verify/verifyPaging", $body);
     }
+
+    /**
+     * 查询项目限制数据
+     * @link https://uniondrug.coding.net/p/module.project/git/blob/development/docs/api/GroupController/groupLimitAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function groupLimit($body)
+    {
+        return $this->restful("POST", "/group/check/limit", $body);
+    }
 }
