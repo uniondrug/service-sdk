a6c0891495570a4e5789412d07710fe0a6d16cca
miffy <zuoqiang320@outlook.com>
2019-11-20 21:53:34 +0800

    修改兑换码接口

--- src/Exports/Modules/ProjectSdk.php
+++ src/Exports/Modules/ProjectSdk.php
@@ -1132,4 +1132,15 @@ class ProjectSdk extends SdkBase
     {
         return $this->restful("POST", "/user/userChangeSingleType", $body);
     }
+
+    /**
+     * changeProjectTypeAction()
+     * @link https://uniondrug.coding.net/p/module.project/git/blob/development/docs/api/UserController/changeProjectTypeAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function userChangeProjectType($body)
+    {
+        return $this->restful("POST", "/user/userChangeProjectType", $body);
+    }
 }
