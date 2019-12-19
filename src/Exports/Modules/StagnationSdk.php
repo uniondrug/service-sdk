99b74cde26183b9ba340c9344334dcef4e2b0d57
William <william_wqq@163.com>
2019-12-19 18:18:32 +0800

    人员迁移接口

--- src/Exports/Modules/StagnationSdk.php
+++ src/Exports/Modules/StagnationSdk.php
@@ -187,6 +187,17 @@ class StagnationSdk extends SdkBase
     }
 
     /**
+     * 迁移
+     * @link https://uniondrug.coding.net/p/module.stagnation/git/blob/development/docs/api/GroupManageController/moveAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function groupManageMove($body)
+    {
+        return $this->restful("POST", "/group/manage/move", $body);
+    }
+
+    /**
      * 获取权益分组占用情况
      * @link https://uniondrug.coding.net/p/module.stagnation/git/blob/development/docs/api/GroupManageController/occupyAction.md
      * @param array $body 入参类型
