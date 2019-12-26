a16ef50bd661e378b57f4fe44b1feceb5b53338f
guoyan <guoyan@uniondrug.net>
2019-12-26 17:52:24 +0800

    casesdk

--- src/Exports/Backend.php
+++ src/Exports/Backend.php
@@ -5,12 +5,14 @@
  */
 namespace Uniondrug\ServiceSdk\Exports;
 
+use Uniondrug\ServiceSdk\Exports\Backends\CaseSdk;
 use Uniondrug\ServiceSdk\Traits\BackendTrait;
 use Uniondrug\ServiceSdk\Exports\Backends\ReservationPhysicalSdk;
 
 /**
  * SDK For Backend
  * @property ReservationPhysicalSdk $reservationPhysical
+ * @property CaseSdk $case
  * @package Uniondrug\ServiceSdk\Exports
  */
 class Backend extends Abstracts\Export
