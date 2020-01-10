2ad4732a61c85c413f50af51753739e9a67526da
miffy <zuoqiang320@outlook.com>
2020-01-10 10:53:29 +0800

    通过订单号和productId获取保障ID和权益ID

--- src/Exports/Unions/ProjectSdk.php
+++ src/Exports/Unions/ProjectSdk.php
@@ -254,4 +254,14 @@ class ProjectSdk extends SdkBase
     {
         return $this->restful("POST", "/verify/ydbCheck", $body);
     }
+    /**
+     * 通过订单号和productId获取保障ID和权益ID
+     * @link https://uniondrug.coding.net/p/union.project/git/blob/development/docs/api/AuditController/auditGetEquityAndGuaranteeAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function auditEquityAndGuarantee($body)
+    {
+        return $this->restful("POST", "/audit/getEquityAndGuarantee", $body);
+    }
 }
