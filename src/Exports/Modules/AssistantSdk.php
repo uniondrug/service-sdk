64fd621cb559647a585fae8f3c86c1ee8f1a25e8
康小强 <kangxiaoqiang@hoge.cn>
2020-03-04 21:08:20 +0800

    zhushou

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -1197,5 +1197,16 @@ class AssistantSdk extends SdkBase
         return $this->restful("POST", "/selfApply/cancelFollow", $body);
     }
 
+    /**
+     * 系统资源合并
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/CloudCustomerController/mergeAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function cloudCustomerMerge($body)
+    {
+        return $this->restful("POST", "/cloudCustomer/merge", $body);
+    }
+
 
 }
