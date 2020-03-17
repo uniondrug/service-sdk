438d1fd84eab9518d814da2509620ffb070ac6ec
钱丞 <qianchengoy@126.com>
2020-03-17 10:17:50 +0800

    增值服务平台服务

---
+++ src/Exports/Modules/VasSdk.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * 重要说明
+ * 1. 本文件由Postman命令脚本自动生成, 请不要修改, 若需修改
+ *    请通过`php console postman`命令重新生成.
+ * 2. 本脚本在生成时, 依赖所在项目的Controller有 `@Sdk method`定义,
+ *    同时, 项目根目录下的`postman.json`需有`sdk`、`sdkLink`定义
+ * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
+ *    的`src/Exports/Modules`目录下，并发重新发布release版本.
+ * @author PostmanCommand
+ * @date   2020-03-17
+ * @time   Tue, 17 Mar 2020 10:14:00 +0800
+ */
+namespace Uniondrug\ServiceSdk\Exports\Modules;
+
+use Uniondrug\ServiceSdk\Exports\Abstracts\SdkBase;
+use Uniondrug\ServiceSdk\Responses\ResponseInterface;
+
+/**
+ * VasSdk
+ * @package Uniondrug\ServiceSdk\Modules
+ */
+class VasSdk extends SdkBase
+{
+    /**
+     * 服务名称
+     * 自来`postman.json`文件定义的`sdkService`值
+     * @var string
+     */
+    protected $serviceName = 'vas.module';
+
+    /**
+     * 产品类型
+     * @link https://uniondrug.coding.net/p/module.vas/git/blob/development/docs/api/GoodsController/getTypeAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function getGoodsType($body)
+    {
+        return $this->restful("POST", "/goods/getType", $body);
+    }
+
+    /**
+     * 产品推送上架
+     * @link https://uniondrug.coding.net/p/module.vas/git/blob/development/docs/api/GoodsController/postDataAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function postGoodsData($body)
+    {
+        return $this->restful("POST", "/goods/postData", $body);
+    }
+
+    /**
+     * 查询产品列表
+     * @link https://uniondrug.coding.net/p/module.vas/git/blob/development/docs/api/GoodsController/queryListAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function queryGoodsList($body)
+    {
+        return $this->restful("POST", "/goods/queryList", $body);
+    }
+
+    /**
+     * 增值服务组包
+     * @link https://uniondrug.coding.net/p/module.vas/git/blob/development/docs/api/PackController/registerOneAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function registerPack($body)
+    {
+        return $this->restful("POST", "/pack/registerOne", $body);
+    }
+}
