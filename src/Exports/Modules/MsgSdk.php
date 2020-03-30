92b9700b16db692d37cf0c8ad2ca915429eda09d
王猛 <1551592188@qq.com>
2020-03-30 17:44:27 +0800

    msg新增短信sdk

--- src/Exports/Modules/MsgSdk.php
+++ src/Exports/Modules/MsgSdk.php
@@ -8,13 +8,13 @@
  * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
- * @date   2019-08-27
- * @time   Tue, 27 Aug 2019 10:50:41 +0800
+ * @date   2020-03-30
+ * @time   Mon, 30 Mar 2020 17:41:51 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
 use Uniondrug\ServiceSdk\Exports\Abstracts\SdkBase;
-use Uniondrug\ServiceSdk\Bases\ResponseInterface;
+use Uniondrug\ServiceSdk\Responses\ResponseInterface;
 
 /**
  * MsgSdk
@@ -49,6 +49,17 @@ class MsgSdk extends SdkBase
     public function publish($body)
     {
         return $this->restful("POST", "/msg/publish", $body);
+    }
+
+    /**
+     * 短信消息发布
+     * @link https://uniondrug.coding.net/p/module.msg/git/blob/development/docs/api/Msg/PublishController/publishSmsAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function publishSms($body)
+    {
+        return $this->restful("POST", "/msg/publishSms", $body);
     }
 
     /**
