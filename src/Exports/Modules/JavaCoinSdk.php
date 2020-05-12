7c6f461efd8e203e7c92acf062571f404432cb19
Mario Zhao <shuo_128@foxmail.com>
2020-05-12 13:53:44 +0800

    资金池变动流水分页列表

--- src/Exports/Modules/JavaCoinSdk.php
+++ src/Exports/Modules/JavaCoinSdk.php
@@ -79,6 +79,16 @@ class JavaCoinSdk extends SdkBase
     }
 
     /**
+     * 资金池变动流水分页列表
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function fundRecordPaging($body)
+    {
+        return $this->restful("POST", "/fundRecord/listByAccountHolder", $body);
+    }
+
+    /**
      * 发起汇款
      * @param $body
      * @return ResponseInterface
