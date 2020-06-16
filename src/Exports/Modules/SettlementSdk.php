9ba9cac683d0aa0b51f9a2359e8b7eaa7f8a1258
meiyfz <1352536432@qq.com>
2020-06-16 15:41:00 +0800

    insert log

--- src/Exports/Modules/SettlementSdk.php
+++ src/Exports/Modules/SettlementSdk.php
@@ -3240,4 +3240,15 @@ class SettlementSdk extends SdkBase
     {
         return $this->restful("POST", "/claimsTpa/removeRelatePolicies", $body);
     }
+
+    /**
+     * 分页列表
+     * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/ClaimLogTpaController/pagingAction.md
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     */
+    public function claimLogTpaPaging($body)
+    {
+        return $this->restful("POST", "/claimLogTpa/paging", $body);
+    }
 }
