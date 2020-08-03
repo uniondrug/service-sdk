10c4fbe121ed751c42e6a25207e3167759c262ea
章满阳 <zhangmanyang@uniondrug.com>
2020-08-03 14:43:41 +0800

    移动检测车- 更新SDK

--- src/Exports/Modules/InspectionSdk.php
+++ src/Exports/Modules/InspectionSdk.php
@@ -8,8 +8,8 @@
  * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
- * @date   2020-07-30
- * @time   Thu, 30 Jul 2020 16:41:24 +0800
+ * @date   2020-08-03
+ * @time   Mon, 03 Aug 2020 14:39:48 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
@@ -651,6 +651,19 @@ class InspectionSdk extends SdkBase
     public function mockOrder($body, $query = null, $extra = null)
     {
         return $this->restful("POST", "/mock/mockOrder", $body, $query, $extra);
+    }
+
+    /**
+     * Mock出库单
+     * @link https://uniondrug.coding.net/p/module.inspection/git/blob/development/docs/api/MockController/mockStockOutAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function mockStockOut($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/mock/stock/out", $body, $query, $extra);
     }
 
     /**
