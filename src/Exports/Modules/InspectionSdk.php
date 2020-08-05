931c6b3814548fa2ce9efaeba3421316e717d0e6
章满阳 <zhangmanyang@uniondrug.com>
2020-08-05 16:46:34 +0800

    移动检测车- 更新SDK

--- src/Exports/Modules/InspectionSdk.php
+++ src/Exports/Modules/InspectionSdk.php
@@ -8,8 +8,8 @@
  * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
- * @date   2020-08-03
- * @time   Mon, 03 Aug 2020 14:39:48 +0800
+ * @date   2020-08-05
+ * @time   Wed, 05 Aug 2020 16:45:45 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
@@ -1080,6 +1080,19 @@ class InspectionSdk extends SdkBase
     public function savePrivilegeMenu($body, $query = null, $extra = null)
     {
         return $this->restful("POST", "/privilege/save/menu", $body, $query, $extra);
+    }
+
+    /**
+     * 保存角色信息
+     * @link https://uniondrug.coding.net/p/module.inspection/git/blob/development/docs/api/PrivilegesController/saveRoleAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function saveRole($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/privilege/save/role", $body, $query, $extra);
     }
 
     /**
