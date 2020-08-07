379c8fecfb7cc787f03494c24fafc81ca078882c
Mario Zhao <shuo_128@foxmail.com>
2020-08-07 10:09:43 +0800

    核算单位级详情

--- src/Exports/Modules/SettlementSdk.php
+++ src/Exports/Modules/SettlementSdk.php
@@ -2920,6 +2920,19 @@ class SettlementSdk extends SdkBase
     }
 
     /**
+     * 核算单位级详情
+     * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/OrganizeFinanceAccountController/unitDetailAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function organizeFinanceAccountUnitDetail($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/organizeFinanceAccount/unitDetail", $body, $query, $extra);
+    }
+
+    /**
      * 分页列表
      * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/OrganizeFinanceAccountController/pagingAction.md
      * @param $body
