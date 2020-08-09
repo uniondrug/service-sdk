da4c43ac280ea4201495ef0233cc6d06edd25105
Mario Zhao <shuo_128@foxmail.com>
2020-08-09 15:48:48 +0800

    核算单位详情

--- src/Exports/Modules/SettlementSdk.php
+++ src/Exports/Modules/SettlementSdk.php
@@ -1655,6 +1655,19 @@ class SettlementSdk extends SdkBase
     }
 
     /**
+     * 核算单位详情
+     * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/OrganizeBankAccountController/unitDetailAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function organizeBankAccountUnitDetail($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/organizeBankAccount/unitDetail", $body, $query, $extra);
+    }
+
+    /**
      * 分页列表
      * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/OrganizeBankAccountController/pagingAction.md
      * @param $body
