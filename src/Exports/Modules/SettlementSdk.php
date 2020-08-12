d81d5ce0400ac9d453e9d74d5f6b2e5c1c6dfdfa
Mario Zhao <shuo_128@foxmail.com>
2020-08-12 14:11:54 +0800

    结算单订单扣率级汇总

--- src/Exports/Modules/SettlementSdk.php
+++ src/Exports/Modules/SettlementSdk.php
@@ -3722,4 +3722,17 @@ class SettlementSdk extends SdkBase
     {
         return $this->restful("POST", "/onlineInvoiceProtocol/create", $body, $query, $extra);
     }
+
+    /**
+     * 结算单订单扣率级汇总
+     * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/StatementsController/ordersSummaryAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function statementOrdersSummary($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/statements/ordersSummary", $body, $query, $extra);
+    }
 }
