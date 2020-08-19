384ac1ff94b56ec737bb4d81fe1d8dcfbf19417b
章满阳 <zhangmanyang@uniondrug.com>
2020-08-19 18:33:51 +0800

    移动检测车- 更新SDK

--- src/Exports/Modules/InspectionSdk.php
+++ src/Exports/Modules/InspectionSdk.php
@@ -8,8 +8,8 @@
  * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
- * @date   2020-08-18
- * @time   Tue, 18 Aug 2020 11:21:12 +0800
+ * @date   2020-08-19
+ * @time   Wed, 19 Aug 2020 18:32:02 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
@@ -183,6 +183,19 @@ class InspectionSdk extends SdkBase
     public function commodityTypeList($body, $query = null, $extra = null)
     {
         return $this->restful("GET", "/stock/commodity-type/list", $body, $query, $extra);
+    }
+
+    /**
+     * 推荐单确认
+     * @link https://uniondrug.coding.net/p/module.inspection/git/blob/development/docs/api/Dispatch/RecommendedNoteController/confirmRecommendedAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function confirmRecommended($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/recommended/confirm", $body, $query, $extra);
     }
 
     /**
@@ -1038,7 +1051,7 @@ class InspectionSdk extends SdkBase
      * @param null $extra  请求头信息
      * @return ResponseInterface
      */
-    public function modifyRecommendedLineList($body, $query = null, $extra = null)
+    public function modifyRecommendeLineList($body, $query = null, $extra = null)
     {
         return $this->restful("POST", "/recommended/modify", $body, $query, $extra);
     }
