e75fe30dd717a047ac0479ce9525fc3bed6ab1af
康小强 <kangxiaoqiang@hoge.cn>
2020-08-20 19:25:37 +0800

    zhushou

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -2489,4 +2489,30 @@ class AssistantSdk extends SdkBase
     {
         return $this->restful("POST", "/storeStatistic/storeCountOfServiceOrder", $body, $query, $extra);
     }
+
+    /**
+     * 管辖店员数统计
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Statistic/UserStatisticController/statisticAssistantCountAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function usersStatisticAssistantCount($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/userStatistic/statisticAssistantCount", $body, $query, $extra);
+    }
+
+    /**
+     * 分页列表 (带统计数据)
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Statistic/UserStatisticController/statisticPagingAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function usersStatisticPaging($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/userStatistic/statisticPaging", $body, $query, $extra);
+    }
 }
