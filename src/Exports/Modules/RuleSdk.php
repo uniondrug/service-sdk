f1cbcd4708892c1f37e56982cb9dc698bd73dd0d
zq <zuoqiang@uniondrug.com>
2020-08-24 09:51:50 +0800

    提交文档

--- src/Exports/Modules/RuleSdk.php
+++ src/Exports/Modules/RuleSdk.php
@@ -615,4 +615,17 @@ class RuleSdk extends SdkBase
     {
         return $this->restful("POST", "/goodsList/update", $body, $query, $extra);
     }
+
+    /**
+     * 商品批量新增
+     * @link https://uniondrug.coding.net/p/module.rule/git/blob/development/module.rule/api/ProgramGoodsController/batchWhiteAddAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function batchWhiteListAdd($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/program/goods/addGoodsAdd", $body, $query, $extra);
+    }
 }
