9bd2106c49ef2203d79ed2b68b2b9b5e26a0d4f6
zq <zuoqiang@uniondrug.com>
2020-08-26 16:57:10 +0800

    提交文档

--- src/Exports/Modules/JavaUserServiceSdk.php
+++ src/Exports/Modules/JavaUserServiceSdk.php
@@ -154,4 +154,15 @@ class JavaUserServiceSdk extends SdkBase
     {
         return $this->restful("POST", "/v2/api/userBasic/addOrQuery", $body);
     }
+
+    /**
+     * 通过身份证类型和memberID查询信息
+     * @link https://uniondrug.coding.net/p/java.middlend.usercenter/d/java.middlend.usercenter/git/tree/feature_api_v1.0.2/api-docs/uniondrug.usercenter.provider/UserBasicApiController/addOrQueryApi.md
+     * @param array|object $body 入参类型
+     * @return ResponseInterface
+     */
+    public function userCardInfoByMemberIdsAndType($body)
+    {
+        return $this->restful("POST", "/v2/api/userCard/batchByIds", $body);
+    }
 }
