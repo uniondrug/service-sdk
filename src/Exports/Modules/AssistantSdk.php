2d1ec49b5dd7f649793bdd30355d016ed68aa4ae
康小强 <kangxiaoqiang@hoge.cn>
2020-09-08 15:18:56 +0800

    zhushou

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -2620,4 +2620,17 @@ class AssistantSdk extends SdkBase
     {
         return $this->restful("POST", "/storeStatistic/orderStoreList", $body, $query, $extra);
     }
+
+    /**
+     * 获取待确认总数
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Approval/ExternalApplyController/countAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function externalApplyCount($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/externalApply/count", $body, $query, $extra);
+    }
 }
