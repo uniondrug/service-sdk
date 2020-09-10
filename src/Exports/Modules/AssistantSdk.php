4775e770e69f6a64198b5a54d4ee8c88f5f86579
康小强 <kangxiaoqiang@hoge.cn>
2020-09-10 13:58:51 +0800

    zhushou

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -2633,4 +2633,18 @@ class AssistantSdk extends SdkBase
     {
         return $this->restful("POST", "/externalApply/count", $body, $query, $extra);
     }
+
+
+    /**
+     * 添加执行记录
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Apply/ApplyController/addErrorMsgAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function applyAddErrorMsg($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/apply/addErrorMsg", $body, $query, $extra);
+    }
 }
