5e3c0a5a770fdd93677c45ba7ecfd64e830e849a
Darrow <346300265@qq.com>
2020-09-14 15:45:04 +0800

    Settlements 添加应收结算单撤销明细接口

--- src/Exports/Modules/SettlementSdk.php
+++ src/Exports/Modules/SettlementSdk.php
@@ -4034,4 +4034,17 @@ class SettlementSdk extends SdkBase
     {
         return $this->restful("POST", "/apply/online/update", $body, $query, $extra);
     }
+
+    /**
+     * 撤销应收结算单待审核明细
+     * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/StatementsReceiveController/cancelAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function statementsReceiveCancel($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/statementsReceive/cancel", $body, $query, $extra);
+    }
 }
