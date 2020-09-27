507f5ac10f4331f5f3f4635a0460477bc95c776a
xujing <xujing1@uniondrug.com>
2020-09-27 15:09:28 +0800

    CompensateSdk增加记录发放失败原因

--- src/Exports/Modules/CompensateSdk.php
+++ src/Exports/Modules/CompensateSdk.php
@@ -1484,4 +1484,17 @@ class CompensateSdk extends SdkBase
     {
         return $this->restful("POST", "/worker/workerInfo", $body, $query, $extra);
     }
+
+    /**
+     * 记录发放失败原因
+     * @link https://uniondrug.coding.net/p/module.compensate/git/blob/development/docs/api/RedeemUserController/updateFailReasonAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function updateFailReason($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/redeemUser/updateFailReason", $body, $query, $extra);
+    }
 }
