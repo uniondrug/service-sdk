47d58e33881c48552845d94f2ad21d3b522f3dce
liuhaifeng <liuhaifeng@uniondrug.com>
2020-09-28 13:51:12 +0800

    商业公司与单体店列表

--- src/Exports/Modules/SettlementSdk.php
+++ src/Exports/Modules/SettlementSdk.php
@@ -4130,4 +4130,17 @@ class SettlementSdk extends SdkBase
     {
         return $this->restful("POST", "/capitalPayment/setAutoRechargeStatus", $body, $query, $extra);
     }
+
+    /**
+     * 不分页列表
+     * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/BusinessSingleUnitController/listAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function businessSingleUnitList($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/businessSingleUnit/businessSingleUnitList", $body, $query, $extra);
+    }
 }
