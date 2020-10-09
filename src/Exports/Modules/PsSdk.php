5303dbf3d4fbb1aec436cdc7227c4d436dd71704
康小强 <kangxiaoqiang@hoge.cn>
2020-10-09 09:42:46 +0800

    pssdk

--- src/Exports/Modules/PsSdk.php
+++ src/Exports/Modules/PsSdk.php
@@ -132,4 +132,18 @@ class PsSdk extends SdkBase
     {
         return $this->restful("POST", "/workStatistic/paging", $body, $query, $extra);
     }
+
+    /**
+     * 用户详细数据
+     * @link https://uniondrug.coding.net/p/bsdata.ps/git/blob/development/docs/api/PharmacyStatisticController/userListAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function pharmacyStatisticUserList($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/pharmacyStatistic/userList", $body, $query, $extra);
+    }
+
 }
