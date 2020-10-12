065135e0841f796ae6b289f39fa22180d9bf72f2
zq <zuoqiang@uniondrug.com>
2020-10-12 10:50:29 +0800

    提交文档

--- src/Exports/Modules/PackageSdk.php
+++ src/Exports/Modules/PackageSdk.php
@@ -8,8 +8,8 @@
  * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
- * @date   2020-10-10
- * @time   Sat, 10 Oct 2020 17:18:41 +0800
+ * @date   2020-10-12
+ * @time   Mon, 12 Oct 2020 10:49:09 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
@@ -184,6 +184,19 @@ class PackageSdk extends SdkBase
     public function guaranteesSpecsDetail($body, $query = null, $extra = null)
     {
         return $this->restful("POST", "/guaranteesSpecs/detail", $body, $query, $extra);
+    }
+
+    /**
+     * 分页
+     * @link https://uniondrug.coding.net/p/module.package/git/blob/development/module.package/api/GuaranteesSpecsController/pagingAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function guaranteesSpecsPaging($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/guaranteesSpecs/paging", $body, $query, $extra);
     }
 
     /**
