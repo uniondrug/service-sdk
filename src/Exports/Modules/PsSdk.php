8463cbe663986067931c59b60b0f871b759e2ae9
康小强 <kangxiaoqiang@hoge.cn>
2020-10-15 10:24:04 +0800

    b端数据

--- src/Exports/Modules/PsSdk.php
+++ src/Exports/Modules/PsSdk.php
@@ -159,4 +159,17 @@ class PsSdk extends SdkBase
         return $this->restful("POST", "/pharmacyStatistic/userLists", $body, $query, $extra);
     }
 
+    /**
+     * 详细数据不分页（日纬度）
+     * @link https://uniondrug.coding.net/p/bsdata.ps/git/blob/development/docs/api/WorKStatisticController/listAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function workStatisticList($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/workStatistic/list", $body, $query, $extra);
+    }
+
 }
