2ce6148f588e1d69e2083ba81fc164a333b5c236
Mario Zhao <shuo_128@foxmail.com>
2020-10-16 16:32:32 +0800

    更新税率

--- src/Exports/Modules/SettlementSdk.php
+++ src/Exports/Modules/SettlementSdk.php
@@ -4204,4 +4204,17 @@ class SettlementSdk extends SdkBase
     {
         return $this->restful("POST", "/applyVip/update", $body, $query, $extra);
     }
+
+    /**
+     * 更新税率
+     * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/ApplyVipController/updateAction.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function orderGoodsUpdateTaxRate($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/orderGoods/updateTaxRate", $body, $query, $extra);
+    }
 }
