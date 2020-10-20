8c49a73ce4f7aade4ef0b93ce8563c6a308962bd
章满阳 <zhangmanyang@uniondrug.com>
2020-10-20 21:45:42 +0800

    移动检测车: 更新 sdk

--- src/Exports/Modules/InspectionSdk.php
+++ src/Exports/Modules/InspectionSdk.php
@@ -9,7 +9,7 @@
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
  * @date   2020-10-20
- * @time   Tue, 20 Oct 2020 17:18:15 +0800
+ * @time   Tue, 20 Oct 2020 21:44:12 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
@@ -326,6 +326,19 @@ class InspectionSdk extends SdkBase
     public function customerOrderList($body, $query = null, $extra = null)
     {
         return $this->restful("POST", "/order/customerlist", $body, $query, $extra);
+    }
+
+    /**
+     * 刷新用户信息
+     * @link https://uniondrug.coding.net/p/module.inspection/git/blob/development/docs/api/LoginController/refreshCustomerTokenAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function customerRefreshToken($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/customer/refresh/token", $body, $query, $extra);
     }
 
     /**
