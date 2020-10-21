6b22d88bdd6b428f1bc50eb627ed01955b199321
章满阳 <zhangmanyang@uniondrug.com>
2020-10-21 11:40:29 +0800

    移动检测车: 更新 sdk

--- src/Exports/Modules/InspectionSdk.php
+++ src/Exports/Modules/InspectionSdk.php
@@ -8,8 +8,8 @@
  * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
- * @date   2020-10-20
- * @time   Tue, 20 Oct 2020 21:44:12 +0800
+ * @date   2020-10-21
+ * @time   Wed, 21 Oct 2020 11:39:48 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
@@ -362,9 +362,22 @@ class InspectionSdk extends SdkBase
      * @param null $extra  请求头信息
      * @return ResponseInterface
      */
-    public function detectionScheduleSave($body, $query = null, $extra = null)
+    public function detectionScheduleRecord($body, $query = null, $extra = null)
     {
         return $this->restful("POST", "/detection/schedule/record", $body, $query, $extra);
+    }
+
+    /**
+     * 检测进度文案管理
+     * @link https://uniondrug.coding.net/p/module.inspection/git/blob/development/docs/api/Detection/DetectionInfoController/saveScheduleAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function detectionScheduleSave($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/detection/schedule/save", $body, $query, $extra);
     }
 
     /**
