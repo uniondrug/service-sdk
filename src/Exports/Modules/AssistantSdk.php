e1c6c7f9226d4ad89df3d1e42fc1730ee50b0667
康小强 <kangxiaoqiang@hoge.cn>
2020-10-23 16:28:41 +0800

    zhushou

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -2815,4 +2815,17 @@ class AssistantSdk extends SdkBase
     {
         return $this->restful("POST", "/approvalInsuranceCompany/create", $body, $query, $extra);
     }
+
+    /**
+     * 使token失效
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/DataService/TokenController/revokeTokenAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function revokeTokensByUserIds($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/token/revokeToken", $body, $query, $extra);
+    }
 }
