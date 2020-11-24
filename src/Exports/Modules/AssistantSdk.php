b25af245aa49a0172766e373529ff6cb759ba03b
康小强 <kangxiaoqiang@hoge.cn>
2020-11-24 15:03:11 +0800

    zhushou

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -2980,4 +2980,17 @@ class AssistantSdk extends SdkBase
     {
         return $this->restful("POST", "/apply/store/deleteByCustomerId", $body, $query, $extra);
     }
+
+    /**
+     * 审批单状态配置
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Apply/ApplyController/applyStatusSettingAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function applyApplyStatusSetting($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/apply/applyStatusSetting", $body, $query, $extra);
+    }
 }
