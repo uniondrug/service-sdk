fc52a8020bbca40b026d4abe251397e9d5b6d2b1
husaiyu <husaiyu@uniondrug.cn>
2020-11-27 09:47:17 +0800

    助手

--- src/Exports/Modules/PsSdk.php
+++ src/Exports/Modules/PsSdk.php
@@ -366,4 +366,17 @@ class PsSdk extends SdkBase
     {
         return $this->restful("POST", "/pharmacyStatistic/userStoreList", $body, $query, $extra);
     }
+
+    /**
+     * 覆盖保司列表接口
+     * @link https://uniondrug.coding.net/p/bsdata.ps/git/blob/development/docs/api/PolicyStatisticController/policyPagingAction.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function policyStatisticPolicyPaging($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/policyStatistic/policyPaging", $body, $query, $extra);
+    }
 }
