7813bd863f613b45edc4c9a4463dd9501034a07b
liuhaifeng <liuhaifeng@uniondrug.com>
2020-12-08 17:17:19 +0800

    财务结算sdk

--- src/Exports/Modules/SettlementSdk.php
+++ src/Exports/Modules/SettlementSdk.php
@@ -4570,4 +4570,17 @@ class SettlementSdk extends SdkBase
     {
         return $this->restful("POST", "/statements/statisticsOrders2", $body, $query, $extra);
     }
+
+    /**
+     * 非药结算单统计
+     * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/StatementsStatisticsController/pagingAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function statementsStatisticsPaging($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/statements/statistics/paging", $body, $query, $extra);
+    }
 }
