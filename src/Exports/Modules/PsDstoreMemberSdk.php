26eb8c003f4c8032c13bf2c34016d9ec4be3c7e7
william-wqq <william_wqq@163.com>
2020-12-11 14:09:02 +0800

    新增JavaImSdk接口

--- src/Exports/Modules/PsDstoreMemberSdk.php
+++ src/Exports/Modules/PsDstoreMemberSdk.php
@@ -860,4 +860,17 @@ class PsDstoreMemberSdk extends SdkBase
     {
         return $this->restful("POST", "/activity/updateBonusUseLog", $body, $query, $extra);
     }
+
+    /**
+     * 更新关注数据
+     * @link https://uniondrug.coding.net/p/ps-dstore-member/git/blob/development/docs/api/ActivityController/updateFollowAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function updateFollow($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/activity/updateFollow", $body, $query, $extra);
+    }
 }
