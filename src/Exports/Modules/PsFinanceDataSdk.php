c78b2e63f3f5f7fa4eea88dadba2a26e9ccde101
Darrow <346300265@qq.com>
2020-12-11 17:49:03 +0800

    更新财务公共数据项目接口

--- src/Exports/Modules/PsFinanceDataSdk.php
+++ src/Exports/Modules/PsFinanceDataSdk.php
@@ -9,7 +9,7 @@
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
  * @date   2020-12-11
- * @time   Fri, 11 Dec 2020 15:32:01 +0800
+ * @time   Fri, 11 Dec 2020 17:48:43 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
@@ -92,6 +92,19 @@ class PsFinanceDataSdk extends SdkBase
     public function bankAccountDetail($body, $query = null, $extra = null)
     {
         return $this->restful("POST", "/bank/account/detail", $body, $query, $extra);
+    }
+
+    /**
+     * 账号详情（含银行及开户行）
+     * @link https://uniondrug.coding.net/p/ps-finance-data/git/blob/development/docs/api/BankAccountController/getDetailAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function bankAccountGetDetail($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/bank/account/get/detail", $body, $query, $extra);
     }
 
     /**
