e5231cb38233fb740d379e05dded14c18ef3a196
liyang <952154830@qq.com>
2020-12-14 18:19:40 +0800

    privilege

--- src/Exports/Modules/PrivilegeSdk.php
+++ src/Exports/Modules/PrivilegeSdk.php
@@ -355,6 +355,19 @@ class PrivilegeSdk extends SdkBase
     }
 
     /**
+     * privilegeCounting
+     * @link https://uniondrug.coding.net/p/module.privilege/git/blob/development/docs/api/Privilege/PrivilegeController/privilegeCountingAction.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function PrivilegeCounting($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/privilege/counting", $body, $query, $extra);
+    }
+
+    /**
      * 后台创建特权任务配置
      * @link https://uniondrug.coding.net/p/module.privilege/git/blob/development/docs/api/Privilege/PrivilegeController/createAction.md
      * @param array|object $body  入参类型
