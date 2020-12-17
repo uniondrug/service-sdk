45a2ebd3f197a9ff33e294c28109600f41eb5173
康小强 <kangxiaoqiang@hoge.cn>
2020-12-17 11:43:10 +0800

    zhushou

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -3097,4 +3097,17 @@ class AssistantSdk extends SdkBase
     {
         return $this->restful("POST", "/operationLog/moduleList", $body, $query, $extra);
     }
+
+    /**
+     * 用户白名单
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/UsersController/userWhiteListAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function userWhiteList($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/users/userWhiteList", $body, $query, $extra);
+    }
 }
