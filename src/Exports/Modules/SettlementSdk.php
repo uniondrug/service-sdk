4f9e17dc763ec96da3594523a5067e81ce03f3e8
liuhaifeng <liuhaifeng@uniondrug.com>
2020-12-24 16:31:40 +0800

    财务结算sdk

--- src/Exports/Modules/SettlementSdk.php
+++ src/Exports/Modules/SettlementSdk.php
@@ -4656,4 +4656,16 @@ class SettlementSdk extends SdkBase
     {
         return $this->restful("POST", "/organizeBill/autoCreate", $body, $query, $extra);
     }
+    /**
+     * 获取待邮寄发票列表-药联理赔单专用
+     * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/InvoiceController/pagingOnWayTpaAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function invoicePagingOnWayTpa($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/invoice/pagingOnWayTpa", $body, $query, $extra);
+    }
 }
