a9a9563d4447960666bf943df17994b9f0a284d4
liuhaifeng <liuhaifeng@uniondrug.com>
2020-12-25 14:26:10 +0800

    fix

--- src/Exports/Modules/SettlementSdk.php
+++ src/Exports/Modules/SettlementSdk.php
@@ -4668,4 +4668,16 @@ class SettlementSdk extends SdkBase
     {
         return $this->restful("POST", "/invoice/pagingOnWayTpa", $body, $query, $extra);
     }
+    /**
+     * 查询核算单位下是否有维护票据信息
+     * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/OrganizeFinanceAccountController/unitRecordAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function organizeFinanceUnitRecord($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/organizeFinanceAccount/unitRecord", $body, $query, $extra);
+    }
 }
