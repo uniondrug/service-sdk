2a297d7bb832d1286fab15a79acca740f19e6319
chenwei <chenwei@uniondrug.cn>
2021-01-05 20:44:12 +0800

    psSdk

--- src/Exports/Modules/PsSdk.php
+++ src/Exports/Modules/PsSdk.php
@@ -457,4 +457,17 @@ class PsSdk extends SdkBase
     {
         return $this->restful("POST", "/work/rank", $body, $query, $extra);
     }
+
+    /**
+     * 大数据各表任务执行情况
+     * @link https://uniondrug.coding.net/p/bsdata.ps/git/blob/development/docs/api/TableDataController/tableDataAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function tableData($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/table/data", $body, $query, $extra);
+    }
 }
