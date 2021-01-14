e485ee989e88b33b62810038623d89ef7a0268a5
Frank Wang <frankwdy11@gmail.com>
2021-01-14 12:34:54 +0800

    误删接口声明恢复

--- src/Exports/Modules/PsFinanceDataSdk.php
+++ src/Exports/Modules/PsFinanceDataSdk.php
@@ -537,6 +537,19 @@ class PsFinanceDataSdk extends SdkBase
     }
 
     /**
+     * 字典分组详情，按名称查询
+     * @link https://uniondrug.coding.net/p/ps-finance-data/git/blob/development/docs/api/DictGroupController/detailByNameAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function dictGroupDetailByName($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/dict/group/detailByName", $body, $query, $extra);
+    }
+
+    /**
      * 无分页列表
      * @link https://uniondrug.coding.net/p/ps-finance-data/git/blob/development/docs/api/DictController/listingAction.md
      * @param array|object $body 入参类型
