2b2b95b5a689bdd0959deac30b000aece4cf377e
chenwei <chenwei@uniondrug.cn>
2021-01-14 15:11:55 +0800

    1

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -3513,4 +3513,17 @@ class AssistantSdk extends SdkBase
     {
         return $this->restful("POST", "/work/chain/contact/positionType", $body, $query, $extra);
     }
+
+    /**
+     * 拜访总次数
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Work/WorkController/visitCountAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function workVisitCount($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/work/visitCount", $body, $query, $extra);
+    }
 }
