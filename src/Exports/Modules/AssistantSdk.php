5b1d3dc23857cc441b60aaf8bade107b1fcc91d9
康小强 <kangxiaoqiang@hoge.cn>
2021-01-16 14:29:21 +0800

    zhushou

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -3606,4 +3606,17 @@ class AssistantSdk extends SdkBase
         return $this->restful("POST", "/work/chain/contact/list", $body, $query, $extra);
     }
 
+    /**
+     * 详情
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Work/WorkRecordVisitController/detailAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function workRecordVisitDetail($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/workRecordVisit/detail", $body, $query, $extra);
+    }
+
 }
