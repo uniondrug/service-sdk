98c75f89f7c43d7af575cf2f2b9224f439842fbb
chenwei <chenwei@uniondrug.cn>
2021-01-16 14:26:37 +0800

    1

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -3592,4 +3592,18 @@ class AssistantSdk extends SdkBase
     {
         return $this->restful("POST", "/approvalTask/workChainVisit", $body, $query, $extra);
     }
+
+    /**
+     * 列表
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Work/WorkChainContactController/listAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function workChainContactList($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/work/chain/contact/list", $body, $query, $extra);
+    }
+
 }
