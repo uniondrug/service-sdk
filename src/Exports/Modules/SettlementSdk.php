4648ed6ea1a02c92c5d1883c844f1c830bfc884a
Mario Zhao <shuo_128@foxmail.com>
2021-01-19 13:28:40 +0800

    核算单位详情

--- src/Exports/Modules/SettlementSdk.php
+++ src/Exports/Modules/SettlementSdk.php
@@ -4221,6 +4221,19 @@ class SettlementSdk extends SdkBase
     }
 
     /**
+     * 核算单位详情
+     * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/ApplyOnlineController/detailAction.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function applyOnlineUnitDetail($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/apply/online/unit/detail", $body, $query, $extra);
+    }
+
+    /**
      * paging
      * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/ApplyOnlineController/pagingAction.md
      * @param array|object $body  入参类型
