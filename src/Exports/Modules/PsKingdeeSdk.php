1b70ff1a9a664ebb850ee2f291ece6464cb71120
Darrow <346300265@qq.com>
2021-01-20 09:53:52 +0800

    添加统计接口

--- src/Exports/Modules/PsKingdeeSdk.php
+++ src/Exports/Modules/PsKingdeeSdk.php
@@ -275,4 +275,17 @@ class PsKingdeeSdk extends SdkBase
     {
         return $this->restful("POST", "/voucher/generate", $body, $query, $extra);
     }
+
+    /**
+     * 无分页列表
+     * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/StatisticsWaitClaimController/listingAction.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function statisticsWaitClaimListing($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/statistics/wait/claim/listing", $body, $query, $extra);
+    }
 }
