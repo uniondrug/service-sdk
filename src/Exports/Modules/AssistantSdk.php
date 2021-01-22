bee8736c109b8e329dfec5c32b51e5bb1e2593b8
康小强 <kangxiaoqiang@hoge.cn>
2021-01-22 16:24:07 +0800

    zhushou

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -3788,4 +3788,18 @@ class AssistantSdk extends SdkBase
     {
         return $this->restful("POST", "/workRecordTrain/list", $body, $query, $extra);
     }
+
+    /**
+     * 新增拜访任务
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Work/WorkRecordImageController/getImagesAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function getWorkRecordImages($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/workRecordImage/getImages", $body, $query, $extra);
+    }
+
 }
