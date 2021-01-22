52625577ce13c807dff8591d9cd13d376f24abda
liuhaifeng <liuhaifeng@uniondrug.com>
2021-01-22 15:15:59 +0800

    财务结算sdk

--- src/Exports/Modules/SettlementSdk.php
+++ src/Exports/Modules/SettlementSdk.php
@@ -4755,4 +4755,16 @@ class SettlementSdk extends SdkBase
     {
         return $this->restful("POST", "/statements/integral/list", $body, $query, $extra);
     }
+    /**
+     * 查询结算账期内积分数额
+     * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/StatementsController/integralAmountAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function statementsIntegralAmount($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/statements/integral/amount", $body, $query, $extra);
+    }
 }
