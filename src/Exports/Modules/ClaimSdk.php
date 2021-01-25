bb02d0db9215e82aca6d760aa1c2f8fd723ea89c
左强 <zuo.qiang@uniondrug.com>
2021-01-25 10:42:19 +0800

    提交文档

--- src/Exports/Modules/ClaimSdk.php
+++ src/Exports/Modules/ClaimSdk.php
@@ -8,8 +8,8 @@
  * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
- * @date   2020-11-25
- * @time   Wed, 25 Nov 2020 10:55:12 +0800
+ * @date   2021-01-25
+ * @time   Mon, 25 Jan 2021 10:34:29 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
@@ -378,6 +378,58 @@ class ClaimSdk extends SdkBase
     public function claimRejectOld($body, $query = null, $extra = null)
     {
         return $this->restful("POST", "/claim/claimRejectOld", $body, $query, $extra);
+    }
+
+    /**
+     * 详情
+     * @link https://uniondrug.coding.net/p/module.claim/git/tree/development/module.claim/api/RenewRuleController/detailAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function claimRenewDetail($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/renew/claim/detail", $body, $query, $extra);
+    }
+
+    /**
+     * 换新理赔人工初审
+     * @link https://uniondrug.coding.net/p/module.claim/git/tree/development/module.claim/api/RenewRuleController/renewFirstTrialAuditAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function claimRenewFirstTrialAudit($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/renew/claim/firstTrial/audit", $body, $query, $extra);
+    }
+
+    /**
+     * 审核列表
+     * @link https://uniondrug.coding.net/p/module.claim/git/tree/development/module.claim/api/RenewRuleController/getRenewPagingAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function claimRenewPaging($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/renew/claim/paging", $body, $query, $extra);
+    }
+
+    /**
+     * 换新复审
+     * @link https://uniondrug.coding.net/p/module.claim/git/tree/development/module.claim/api/RenewRuleController/renewRecheckAuditAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function claimRenewRecheckAudit($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/renew/claim/recheck/auidt", $body, $query, $extra);
     }
 
     /**
