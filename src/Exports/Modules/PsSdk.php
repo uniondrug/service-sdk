f2c1bc4a7265b7dd74325e3b917f5f83e47b360f
chenwei <chenwei@uniondrug.cn>
2021-01-29 14:44:34 +0800

    psSDK

--- src/Exports/Modules/PsSdk.php
+++ src/Exports/Modules/PsSdk.php
@@ -483,4 +483,17 @@ class PsSdk extends SdkBase
     {
         return $this->restful("POST", "/work/storeDistributed", $body, $query, $extra);
     }
+
+    /**
+     * 活跃奖励
+     * @link https://uniondrug.coding.net/p/bsdata.ps/git/blob/development/docs/api/WorkController/bonusAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function workBonus($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/work/bonus", $body, $query, $extra);
+    }
 }
