0d6dda70d2e36cb77f5df21da09337cf648bd1a6
Haven <chenhao@uniondrug.com>
2021-02-23 19:47:18 +0800

    公共配置保司开票分页

--- src/Exports/Modules/PsFinanceDataSdk.php
+++ src/Exports/Modules/PsFinanceDataSdk.php
@@ -641,6 +641,19 @@ class PsFinanceDataSdk extends SdkBase
     }
 
     /**
+     * 保司开票分页列表
+     * @link https://uniondrug.coding.net/p/ps-finance-data/git/blob/development/docs/api/Insurer/BillInfoController/pagingAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function insurerBillInfoPaging($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/insurer/bill/info/paging", $body, $query, $extra);
+    }
+
+    /**
      * 新增保司开票
      * @link https://uniondrug.coding.net/p/ps-finance-data/git/blob/development/docs/api/Insurer/BillInfoController/createAction.md
      * @param array|object $body  入参类型
