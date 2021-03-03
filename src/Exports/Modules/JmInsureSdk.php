013c11bfd85c98b939a223e753eda6bbec237bfc
Mario Zhao <shuo_128@foxmail.com>
2021-03-03 10:03:02 +0800

    停用方案关联保司组

--- src/Exports/Modules/JmInsureSdk.php
+++ src/Exports/Modules/JmInsureSdk.php
@@ -149,4 +149,186 @@ class JmInsureSdk extends SdkBase
     {
         return $this->restful("POST", "/ipackage/exportClaim", $body);
     }
+
+    /**
+     * 方案创建
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/SchemeController/save.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function schemeCreate($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/scheme/save", $body);
+    }
+
+    /**
+     * 方案更新
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/SchemeController/update.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function schemeUpdate($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/scheme/update", $body);
+    }
+
+    /**
+     * 方案详情
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/SchemeController/get.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function schemeGet($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/scheme/get", $body);
+    }
+
+    /**
+     * 获取方案编号
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/SchemeController/getSchemeNo.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function getSchemeNo($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/scheme/getSchemeNo", $body);
+    }
+
+    /**
+     * 方案分页
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/SchemeController/page.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function schemePaging($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/scheme/page", $body);
+    }
+
+    /**
+     * 方案移出
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/SchemeController/remove.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function schemeRemove($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/scheme/remove", $body);
+    }
+
+    /**
+     * 启用方案
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/SchemeController/enable.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function schemeEnable($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/scheme/enable", $body);
+    }
+
+    /**
+     * 停用方案
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/SchemeController/disable.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function schemeDisable($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/scheme/disable", $body);
+    }
+
+    /**
+     * 保存分配保司
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/SchemeInsurerController/save.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function schemeInsurerCreate($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/schemeInsurer/save", $body);
+    }
+
+    /**
+     * 更新分配保司
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/SchemeInsurerController/update.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function schemeInsurerUpdate($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/schemeInsurer/update", $body);
+    }
+
+    /**
+     * 方案关联保司组分页
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/SchemeInsurerController/page.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function schemeInsurerPaging($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/schemeInsurer/page", $body);
+    }
+
+    /**
+     * 删除方案关联保司组
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/SchemeInsurerController/remove.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function schemeInsurerRemove($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/schemeInsurer/remove", $body);
+    }
+
+    /**
+     * 启用方案关联保司组
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/SchemeInsurerController/enable.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function schemeInsurerEnable($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/schemeInsurer/enable", $body);
+    }
+
+    /**
+     * 停用方案关联保司组
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/SchemeInsurerController/disable.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function schemeInsurerDisable($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/schemeInsurer/disable", $body);
+    }
 }
