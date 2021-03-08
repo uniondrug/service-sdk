26714705f06eeaba5a23e2237e63f9dd4bca9458
Haven <chenhao@uniondrug.com>
2021-03-08 13:09:02 +0800

    公共数据保司列表接口

--- src/Exports/Modules/PsFinanceDataSdk.php
+++ src/Exports/Modules/PsFinanceDataSdk.php
@@ -667,6 +667,19 @@ class PsFinanceDataSdk extends SdkBase
     }
 
     /**
+     * 保司列表
+     * @link https://uniondrug.coding.net/p/ps-finance-data/git/blob/development/docs/api/Insurer/InsurerController/listingAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function insurerListing($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/insurer/listing", $body, $query, $extra);
+    }
+
+    /**
      * 保司分页列表
      * @link https://uniondrug.coding.net/p/ps-finance-data/git/blob/development/docs/api/InsurerController/pagingAction.md
      * @param array|object $body  入参类型
