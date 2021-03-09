75d2c2aaf62c3faac9639178e716be5b3af689a0
康小强 <kangxiaoqiang@hoge.cn>
2021-03-09 14:20:55 +0800

    zhushou

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -4137,4 +4137,17 @@ class AssistantSdk extends SdkBase
     {
         return $this->restful("POST", "/workRecord/getByWorkId", $body, $query, $extra);
     }
+
+    /**
+     * 完成任务
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Work/WorkRecordController/completeAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function workRecordComplete($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/workRecord/complete", $body, $query, $extra);
+    }
 }
