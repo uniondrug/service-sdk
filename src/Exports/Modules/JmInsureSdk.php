4f188b15a8ce91d47bfaeece950730be6d48dcb1
Mario Zhao <shuo_128@foxmail.com>
2021-03-13 14:31:06 +0800

    exportClaim

--- src/Exports/Modules/JmInsureSdk.php
+++ src/Exports/Modules/JmInsureSdk.php
@@ -164,6 +164,32 @@ class JmInsureSdk extends SdkBase
     }
 
     /**
+     * 药联理赔单收款申请
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/IpackageController/uniondrugReceiptSave.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function unionDrugClaimReceiptApply($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/ipackage/uniondrug/receiptSave", $body);
+    }
+
+    /**
+     * 药联理赔单开票申请
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/IpackageController/uniondrugInvoiceApply.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function unionDrugClaimBillApply($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/ipackage/uniondrug/invoiceApply", $body);
+    }
+
+    /**
      * 待理赔池汇总分页查询
      * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/PoolClaimSummaryController/page.md
      * @param array|object $body  入参类型
@@ -216,8 +242,47 @@ class JmInsureSdk extends SdkBase
     }
 
     /**
+     * 提交理赔单
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/IpackageController/commit.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function claimSubmit($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/ipackage/commit", $body);
+    }
+
+    /**
+     * 撤销理赔单
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/IpackageController/undo.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function claimUndo($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/ipackage/undo", $body);
+    }
+
+    /**
+     * 完成赔付理赔单
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/IpackageController/complete.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function claimComplete($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/ipackage/complete", $body);
+    }
+
+    /**
      * 理赔单导出理赔数据
-     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/IpackageLogController/page.md
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/IpackageLogController/exportClaim.md
      * @param array|object $body  入参类型
      * @param null         $query Query数据
      * @param null         $extra 请求头信息
@@ -421,6 +486,19 @@ class JmInsureSdk extends SdkBase
     public function schemeInsurerDisable($body, $query = null, $extra = null)
     {
         return $this->restful("POST", "/schemeInsurer/disable", $body);
+    }
+
+    /**
+     * 投保流量额
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/InsureController/pool.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function estimateAmount($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/insure/pool", $body);
     }
 
     /**
