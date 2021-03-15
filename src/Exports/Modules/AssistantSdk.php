2674d9e837035a082fa8687fe0ef603179bb42f5
markwang1992 <617514823@qq.com>
2021-03-15 14:06:18 +0800

    任务分配新增

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -4189,4 +4189,17 @@ class AssistantSdk extends SdkBase
     {
         return $this->restful("POST", "/assistantRemark/idsList", $body, $query, $extra);
     }
+
+    /**
+     * 新增
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Work/WorkDistributeController/createAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function workDistributeCreate($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/workDistribute/create", $body, $query, $extra);
+    }
 }
