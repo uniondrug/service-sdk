538282ccca9909eba82766b75543f8106cefdc78
chenwei <chenwei@uniondrug.cn>
2021-03-15 15:44:19 +0800

    assistantSDK

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -4202,4 +4202,17 @@ class AssistantSdk extends SdkBase
     {
         return $this->restful("POST", "/workDistribute/create", $body, $query, $extra);
     }
+
+    /**
+     * 批量删除
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Work/WorkController/deleteAction.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function workDelete($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/work/delete", $body, $query, $extra);
+    }
 }
