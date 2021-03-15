7ec1e831cade7970fa198b8d2f0b4e723e26df41
chenwei <chenwei@uniondrug.cn>
2021-03-15 22:43:56 +0800

    assistantSDK

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -4254,4 +4254,17 @@ class AssistantSdk extends SdkBase
     {
         return $this->restful("POST", "/work/delete", $body, $query, $extra);
     }
+
+    /**
+     * 任务完成数据
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Work/WorkController/finishCountAction.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function workFinishCount($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/work/finishCount", $body, $query, $extra);
+    }
 }
