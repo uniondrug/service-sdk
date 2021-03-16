394eab2c1fdd635be6741d16a54efbd2750fff14
Mario Zhao <shuo_128@foxmail.com>
2021-03-16 16:44:24 +0800

    partnerWorkerManDetail

--- src/Exports/Modules/PsFinanceDataSdk.php
+++ src/Exports/Modules/PsFinanceDataSdk.php
@@ -1392,6 +1392,19 @@ class PsFinanceDataSdk extends SdkBase
      */
     public function partnerWorkerDetail($body, $query = null, $extra = null)
     {
+        return $this->restful("POST", "/partner/worker/member/detail", $body, $query, $extra);
+    }
+
+    /**
+     * 连锁用户详情
+     * @link https://uniondrug.coding.net/p/ps-finance-data/git/blob/development/docs/api/Partner/WorkerController/detailAction.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function partnerWorkerManDetail($body, $query = null, $extra = null)
+    {
         return $this->restful("POST", "/partner/worker/detail", $body, $query, $extra);
     }
 
