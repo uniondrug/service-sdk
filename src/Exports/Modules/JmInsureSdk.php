a91563d19b57d95a8405099860eac310b90beff2
Mario Zhao <shuo_128@foxmail.com>
2021-03-19 17:58:23 +0800

    首页关联查询保单支付计划列表

--- src/Exports/Modules/JmInsureSdk.php
+++ src/Exports/Modules/JmInsureSdk.php
@@ -955,4 +955,17 @@ class JmInsureSdk extends SdkBase
     {
         return $this->restful("POST", "/policy/pageStat", $body);
     }
+
+    /**
+     * 首页关联查询保单支付计划列表
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/PolicyPayController/indexList.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function policyPayIndexList($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "policyPay/index/list", $body);
+    }
 }
