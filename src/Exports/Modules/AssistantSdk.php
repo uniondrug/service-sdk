4e2ba3c79a7d28b41ef2fdb75d27008880bc29bd
chenwei <chenwei@uniondrug.cn>
2021-03-24 09:12:35 +0800

    assistantSDK

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -4346,4 +4346,30 @@ class AssistantSdk extends SdkBase
     {
         return $this->restful("POST", "/userPartners/listing", $body, $query, $extra);
     }
+
+    /**
+     * 详情
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/WorkStoreDistributionSettingController/detailAction.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function workStoreDistributionSettingDetail($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/work/storeDistribution/setting/detail", $body, $query, $extra);
+    }
+
+    /**
+     * 修改
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/WorkStoreDistributionSettingController/updateAction.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function workStoreDistributionSettingUpdate($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/work/storeDistribution/setting/update", $body, $query, $extra);
+    }
 }
