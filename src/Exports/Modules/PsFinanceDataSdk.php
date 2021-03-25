bb2bba5234f81a1b330da24f52c7980240ef61ce
Mario Zhao <shuo_128@foxmail.com>
2021-03-25 16:32:26 +0800

    更新操作人

--- src/Exports/Modules/PsFinanceDataSdk.php
+++ src/Exports/Modules/PsFinanceDataSdk.php
@@ -1513,6 +1513,19 @@ class PsFinanceDataSdk extends SdkBase
     }
 
     /**
+     * 更新操作人
+     * @link https://uniondrug.coding.net/p/ps-finance-data/git/blob/development/docs/api/Partner/WorkerController/pagingAction.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function workerUpdateOperator($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/worker/update/operator", $body, $query, $extra);
+    }
+
+    /**
      * 用户更新选中
      * @link https://uniondrug.coding.net/p/ps-finance-data/git/blob/development/docs/api/Partner/WorkerController/pagingAction.md
      * @param array|object $body  入参类型
