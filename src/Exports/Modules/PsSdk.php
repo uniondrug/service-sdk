044d46ff51ac59aed68c14a701917b64feb71d3c
chenwei <chenwei@uniondrug.cn>
2021-03-30 17:03:03 +0800

    psSDK

--- src/Exports/Modules/PsSdk.php
+++ src/Exports/Modules/PsSdk.php
@@ -434,6 +434,19 @@ class PsSdk extends SdkBase
     }
 
     /**
+     * 门店各标签数量
+     * @link https://uniondrug.coding.net/p/bsdata.ps/git/blob/development/docs/api/WorkController/storeCountAction.md
+     * @param array|object $body 入参类型
+     * @param null $query Query数据
+     * @param null $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function workStoreCount($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/work/storeCount", $body, $query, $extra);
+    }
+
+    /**
      * 业务汇总
      * @link https://uniondrug.coding.net/p/bsdata.ps/git/blob/development/docs/api/WorkController/dataAction.md
      * @param array|object $body 入参类型
