e4f52fb90778c90c7c8b81c77174eb67ed202c53
markwang1992 <617514823@qq.com>
2021-04-09 09:26:23 +0800

    assistantSDK

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -4528,4 +4528,17 @@ class AssistantSdk extends SdkBase
     {
         return $this->restful("POST", "/workStoreGoal/update", $body, $query, $extra);
     }
+
+    /**
+     * 连锁任务自动派发
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Work/WorkDistributeAutoController/chainJobAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function chainJob($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/workDistributeAuto/chainJob", $body, $query, $extra);
+    }
 }
