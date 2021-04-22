bbf251fc8bd47add4dc7bbcd617a10404f0edfc1
Darrow <346300265@qq.com>
2021-04-22 15:58:13 +0800

    添加发票签收检查接口

--- src/Exports/Modules/SettlementSdk.php
+++ src/Exports/Modules/SettlementSdk.php
@@ -4960,4 +4960,17 @@ class SettlementSdk extends SdkBase
     {
         return $this->restful("POST", "/invoice/receive/transaction/update", $body, $query, $extra);
     }
+
+    /**
+     * 检查
+     * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/InvoiceReceiveTransactionDetailController/checkAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function invoiceReceiveTransactionDetailCheck($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/invoice/receive/transaction/detail/cehck", $body, $query, $extra);
+    }
 }
