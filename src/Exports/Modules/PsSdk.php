8dc6967df54eed19a35d9330e10cc5c56d9293a3
markwang1992 <617514823@qq.com>
2021-04-27 10:36:02 +0800

    PsSDK

--- src/Exports/Modules/PsSdk.php
+++ src/Exports/Modules/PsSdk.php
@@ -680,4 +680,17 @@ class PsSdk extends SdkBase
     {
         return $this->restful("POST", "/storeOperationInfoEd/locationLackStorePaging", $body, $query, $extra);
     }
+
+    /**
+     * 批量获取门店
+     * @link https://uniondrug.coding.net/p/bsdata.ps/git/blob/development/docs/api/StoreOperationInfoEdController/getByStoreIdsAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function getByStoreIds($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/storeOperationInfoEd/getByStoreIds", $body, $query, $extra);
+    }
 }
