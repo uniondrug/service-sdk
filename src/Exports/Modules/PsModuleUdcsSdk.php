39c705d383d7e3b9bf2707e6dac0bc2994a594b6
weng <shushun@uniondrug.cn>
2021-05-08 11:20:18 +0800

    商户中心更新 1.增加ps-module-udcs

--- src/Exports/Modules/PsModuleUdcsSdk.php
+++ src/Exports/Modules/PsModuleUdcsSdk.php
@@ -9,7 +9,7 @@
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
  * @date   2021-05-08
- * @time   Sat, 08 May 2021 10:00:38 +0800
+ * @time   Sat, 08 May 2021 11:19:58 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
@@ -79,6 +79,19 @@ class PsModuleUdcsSdk extends SdkBase
     public function detailBatchDetailed($body, $query = null, $extra = null)
     {
         return $this->restful("POST", "/batchDetailed/detail", $body, $query, $extra);
+    }
+
+    /**
+     * 详情
+     * @link https://uniondrug.coding.net/p/ps-module-udcs/git/blob/development/docs/api/CrowdsourcingBatchController/detailAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function detailCrowdsourcingBatch($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/crowdsourcingBatch/detail", $body, $query, $extra);
     }
 
     /**
