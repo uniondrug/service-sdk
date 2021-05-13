02c10927fa29ffcbd2b0bd1a5fc725b019d52d5b
xujing <xujing1@uniondrug.com>
2021-05-13 10:30:08 +0800

    PsDstoreMemberSdk更新

--- src/Exports/Modules/PsDstoreMemberSdk.php
+++ src/Exports/Modules/PsDstoreMemberSdk.php
@@ -2745,4 +2745,17 @@ class PsDstoreMemberSdk extends SdkBase
     {
         return $this->restful("POST", "/starPharmacist/updateStarPharmacist", $body, $query, $extra);
     }
+
+    /**
+     * 根据评价id批量查询
+     * @link https://uniondrug.coding.net/p/ps-dstore-member/git/blob/development/docs/api/EvaluateController/idsAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function queryEvaluateByIds($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/evaluate/ids", $body, $query, $extra);
+    }
 }
