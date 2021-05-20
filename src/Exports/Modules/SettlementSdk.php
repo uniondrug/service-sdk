020772b5c57fbd1c74bf87d925434f1801ba0966
Haven <chenhao@uniondrug.com>
2021-05-20 16:20:13 +0800

    Update SettlementSdk.php
    应收开票单邮寄

--- src/Exports/Modules/SettlementSdk.php
+++ src/Exports/Modules/SettlementSdk.php
@@ -4221,6 +4221,19 @@ class SettlementSdk extends SdkBase
     }
 
     /**
+     * 应收开票单邮寄
+     * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/OrganizeBillController/organizeBillSendAction.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function organizeBillSend($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/organizeBill/organize/bill/send", $body, $query, $extra);
+    }
+
+    /**
      * 新增
      * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/ApplyOnlineController/createAction.md
      * @param array|object $body  入参类型
