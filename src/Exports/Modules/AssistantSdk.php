26b16e4ec1979ab500d74c78737ee833511fc5a3
chenwei <chenwei@uniondrug.cn>
2021-05-20 14:16:57 +0800

    assistantSDK

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -4737,4 +4737,17 @@ class AssistantSdk extends SdkBase
     {
         return $this->restful("POST", "/storeAddrEditApprove/status", $body, $query, $extra);
     }
+
+    /**
+     * 节假日列表
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/HolidaysController/listAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function holidaysList($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/holidays/list", $body, $query, $extra);
+    }
 }
