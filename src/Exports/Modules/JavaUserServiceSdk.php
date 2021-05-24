1ddcf7bde4060ad53c38a7241e0567f0fbd4561c
吕朝晖 <lvchaohui@uniondrug.cn>
2021-05-24 14:10:26 +0800

    1

--- src/Exports/Modules/JavaUserServiceSdk.php
+++ src/Exports/Modules/JavaUserServiceSdk.php
@@ -226,4 +226,28 @@ class JavaUserServiceSdk extends SdkBase
     {
         return $this->restful("POST", "/v2/api/userThird/query", $body, $query, $extra);
     }
+
+    /**
+     * 获取签名
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function userRuleGetSign($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/v2/api/userRule/getSign", $body, $query, $extra);
+    }
+
+    /**
+     * 获取规则
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function userRuleQuery($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/v2/api/userRule/query", $body, $query, $extra);
+    }
 }
