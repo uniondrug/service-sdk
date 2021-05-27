5c9b38922d9ca58bfcd3575212baca1216f37e48
Mario Zhao <shuo_128@foxmail.com>
2021-05-27 11:38:44 +0800

    用户系统更新

--- src/Exports/Modules/PsFinanceDataSdk.php
+++ src/Exports/Modules/PsFinanceDataSdk.php
@@ -1747,6 +1747,19 @@ class PsFinanceDataSdk extends SdkBase
     }
 
     /**
+     * 用户系统更新
+     * @link https://uniondrug.coding.net/p/ps-finance-data/git/blob/development/docs/api/WorkerRuleController/updateAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function workerSystemUpdateStatus($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/worker/system/update/status", $body, $query, $extra);
+    }
+
+    /**
      * 新增商业公司与商户
      * @link https://uniondrug.coding.net/p/ps-finance-data/git/blob/development/docs/api/Relationship/RelationshipController/createAction.md
      * @param array|object $body  入参类型
