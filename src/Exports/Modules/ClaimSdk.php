30b05186b4ac207c44129861900d67f41b3669ec
zong <zongmh@uniondrug>
2021-05-28 13:27:01 +0800

    新增药品检查接口

--- src/Exports/Modules/ClaimSdk.php
+++ src/Exports/Modules/ClaimSdk.php
@@ -8,8 +8,8 @@
  * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
- * @date   2021-01-27
- * @time   Wed, 27 Jan 2021 16:41:44 +0800
+ * @date   2021-05-28
+ * @time   Fri, 28 May 2021 13:24:35 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
@@ -105,6 +105,19 @@ class ClaimSdk extends SdkBase
     public function changeReject($body, $query = null, $extra = null)
     {
         return $this->restful("POST", "/change/reject", $body, $query, $extra);
+    }
+
+    /**
+     * 换新药品检查
+     * @link https://uniondrug.coding.net/p/module.claim/git/tree/development/module.claim/api/ChangeController/checkDrugAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function checkDrug($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/change/checkDrug", $body, $query, $extra);
     }
 
     /**
