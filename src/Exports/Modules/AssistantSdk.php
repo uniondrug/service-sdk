0c3743bbfb0ea28879e444bc85b7c4e54c51c068
康小强 <kangxiaoqiang@hoge.cn>
2021-05-31 14:44:24 +0800

    zhushou

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -4999,4 +4999,43 @@ class AssistantSdk extends SdkBase
     {
         return $this->restful("POST", "/workInsuranceCustomer/list", $body, $query, $extra);
     }
+
+    /**
+     * 新增
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Approval/ApprovalFormTplController/createAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function approvalFormTplCreate($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/approvalFormTpl/create", $body, $query, $extra);
+    }
+
+    /**
+     * 详情
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Approval/ApprovalFormTplController/detailByApproveTypeAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function approvalFormTplDetailByApproveType($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/approvalFormTpl/detailByApproveType", $body, $query, $extra);
+    }
+
+    /**
+     * 更新
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Approval/ApprovalFormTplController/updateAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function approvalFormTplUpdate($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/approvalFormTpl/update", $body, $query, $extra);
+    }
 }
