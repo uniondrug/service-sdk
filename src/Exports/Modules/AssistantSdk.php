b2382f347146912dc3a619a08e6a6acc49bf67e8
markwang1992 <617514823@qq.com>
2021-06-07 18:13:15 +0800

    assistantSDK

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -4714,6 +4714,19 @@ class AssistantSdk extends SdkBase
     }
 
     /**
+     * 门店ID批量获取审批记录数
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Merchant/StoreAddrEditApproveController/getCountByStoreIdsAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function storeAddrEditApproveGetCountByStoreIds($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/storeAddrEditApprove/getCountByStoreIds", $body, $query, $extra);
+    }
+
+    /**
      * 分页列表
      * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Merchant/StoreAddrEditApproveController/pagingAction.md
      * @param array|object $body 入参类型
