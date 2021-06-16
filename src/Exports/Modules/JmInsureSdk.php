00abe7f7b93e17bee9465e139a5c11afbc19d6a8
Haven <chenhao@uniondrug.com>
2021-06-16 15:33:34 +0800

    Update JmInsureSdk.php
    理赔单关联商品分页接口

--- src/Exports/Modules/JmInsureSdk.php
+++ src/Exports/Modules/JmInsureSdk.php
@@ -268,6 +268,19 @@ class JmInsureSdk extends SdkBase
     }
 
     /**
+     * 理赔单关联商品分页查询
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/IpackageOrderController/page.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function claimGoodsPaging($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/equityGoods/page", $body);
+    }
+
+    /**
      * 理赔操作日志分页查询
      * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/IpackageLogController/page.md
      * @param array|object $body  入参类型
