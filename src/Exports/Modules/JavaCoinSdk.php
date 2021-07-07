d8d3495d7ce51ed3b3d6b8cb334d50c082d8cd17
liuhaifeng <liuhaifeng@uniondrug.com>
2021-07-07 15:33:12 +0800

    财务sdk

--- src/Exports/Modules/JavaCoinSdk.php
+++ src/Exports/Modules/JavaCoinSdk.php
@@ -624,4 +624,14 @@ class JavaCoinSdk extends SdkBase
     {
         return $this->restful("POST", "/fundAccount/get", $body);
     }
+
+    /**
+     * 撤销充值申请
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function receiptRevoke($body)
+    {
+        return $this->restful("POST", "/receipt/auditRetract", $body);
+    }
 }
