0da43485df2ab75a1fa6df6bf4d9c875701a9f15
william-wqq <william_wqq@163.com>
2021-07-14 17:11:11 +0800

    新增JavaOrderSdk接口

--- src/Exports/Modules/TakeSdk.php
+++ src/Exports/Modules/TakeSdk.php
@@ -186,6 +186,19 @@ class TakeSdk extends SdkBase
     }
 
     /**
+     * 订单统计
+     * @link https://uniondrug.coding.net/p/module.take/git/tree/development/app/docs/api/OrderController/indexAction.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function orderIndex($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/order/index", $body, $query, $extra);
+    }
+
+    /**
      * 订单列表
      * @link https://uniondrug.coding.net/p/module.take/git/tree/development/app/docs/api/OrderController/pagingAction.md
      * @param array|object $body  入参类型
