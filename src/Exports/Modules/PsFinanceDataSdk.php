30ba06d6e61845254ed875f59d9d6eb6cf959f0d
Mario Zhao <shuo_128@foxmail.com>
2021-07-20 11:57:27 +0800

    编辑连锁用户

--- src/Exports/Modules/PsFinanceDataSdk.php
+++ src/Exports/Modules/PsFinanceDataSdk.php
@@ -1513,6 +1513,19 @@ class PsFinanceDataSdk extends SdkBase
     }
 
     /**
+     * 编辑连锁用户
+     * @link https://uniondrug.coding.net/p/ps-finance-data/git/blob/development/docs/api/Partner/WorkerController/createAction.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function partnerWorkerUpdate($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/partner/worker/update", $body, $query, $extra);
+    }
+
+    /**
      * 连锁用户详情
      * @link https://uniondrug.coding.net/p/ps-finance-data/git/blob/development/docs/api/Partner/WorkerController/detailAction.md
      * @param array|object $body  入参类型
