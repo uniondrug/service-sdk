6c08b6abd50fcabba0bb1825abe7eec29216c345
Mario Zhao <shuo_128@foxmail.com>
2021-07-20 10:03:02 +0800

    核算单位下开启微信推送用户列表

--- src/Exports/Modules/PsFinanceDataSdk.php
+++ src/Exports/Modules/PsFinanceDataSdk.php
@@ -1565,6 +1565,19 @@ class PsFinanceDataSdk extends SdkBase
     }
 
     /**
+     * 核算单位下开启微信推送用户列表
+     * @link https://uniondrug.coding.net/p/ps-finance-data/git/blob/development/docs/api/Partner/WorkerController/detailAction.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function partnerWorkerWeChatPushList($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/partner/worker/wechat/push/list", $body, $query, $extra);
+    }
+
+    /**
      * 连锁用户更新状态
      * @link https://uniondrug.coding.net/p/ps-finance-data/git/blob/development/docs/api/Partner/WorkerController/detailAction.md
      * @param array|object $body  入参类型
