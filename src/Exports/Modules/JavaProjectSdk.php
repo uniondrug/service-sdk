f13d25e5dd5a1de9da0f218bbaa9bddee2d3187c
zong <zongmh@uniondrug>
2021-08-05 15:00:36 +0800

    新增sdk

--- src/Exports/Modules/JavaProjectSdk.php
+++ src/Exports/Modules/JavaProjectSdk.php
@@ -31,4 +31,64 @@ class JavaProjectSdk extends SdkBase
     {
         return $this->restful("POST", "/code/autoCheck", $body);
     }
+
+    /**
+     * 创建激活设置
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function activateSettingCreate($body)
+    {
+        return $this->restful("POST", "/activate/setting/create", $body);
+    }
+
+    /**
+     * 查看激活设置详情
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function activateSettingDetail($body)
+    {
+        return $this->restful("POST", "/activate/setting/detail", $body);
+    }
+
+    /**
+     * 激活设置列表（带分页）
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function activateSettingPaging($body)
+    {
+        return $this->restful("POST", "/activate/setting/paging", $body);
+    }
+
+    /**
+     * 启用配置
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function activateSettingEnable($body)
+    {
+        return $this->restful("POST", "/activate/setting/enable", $body);
+    }
+
+    /**
+     * 停用配置
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function activateSettingDisable($body)
+    {
+        return $this->restful("POST", "/activate/setting/disable", $body);
+    }
+
+    /**
+     * 根据项目或批次查询配置
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function activateSettingGetByPurchaseProjectIdAndPurchasePileId($body)
+    {
+        return $this->restful("POST", "/activate/setting/getByPurchaseProjectIdAndPurchasePileId", $body);
+    }
 }
