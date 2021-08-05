f9e5ca0e9dae4d680fe8d3b1ba63a485c1892d44
markwang1992 <617514823@qq.com>
2021-08-05 10:38:17 +0800

    psSDK

--- src/Exports/Modules/PsSdk.php
+++ src/Exports/Modules/PsSdk.php
@@ -808,4 +808,17 @@ class PsSdk extends SdkBase
     {
         return $this->restful("POST", "/merchantOrdSumD/getByMerchantId", $body, $query, $extra);
     }
+
+    /**
+     * 根据连锁ID获取记录
+     * @link https://uniondrug.coding.net/p/bsdata.ps/git/blob/development/docs/api/UserMerchantDController/getByMerchantIdAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function UMDGetByMerchantId($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/userMerchantD/getByMerchantId", $body, $query, $extra);
+    }
 }
