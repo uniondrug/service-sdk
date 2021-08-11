e18f17bc0387e570a822b8b70402c1cf17830124
markwang1992 <617514823@qq.com>
2021-08-11 17:27:03 +0800

    scrmSDK

---
+++ src/Exports/Modules/PsScrmPulpSdk.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * 重要说明
+ * 1. 本文件由Postman命令脚本自动生成, 请不要修改, 若需修改
+ *    请通过`php console postman`命令重新生成.
+ * 2. 本脚本在生成时, 依赖所在项目的Controller有 `@Sdk method`定义,
+ *    同时, 项目根目录下的`postman.json`需有`sdk`、`sdkLink`定义
+ * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
+ *    的`src/Exports/Modules`目录下，并发重新发布release版本.
+ * @author PostmanCommand
+ * @date   2021-08-11
+ * @time   Wed, 11 Aug 2021 17:22:46 +0800
+ */
+namespace Uniondrug\ServiceSdk\Exports\Modules;
+
+use Uniondrug\ServiceSdk\Exports\Abstracts\SdkBase;
+use Uniondrug\ServiceSdk\Bases\ResponseInterface;
+
+/**
+ * PsScrmPulpSdk
+ * @package Uniondrug\ServiceSdk\Modules
+ */
+class PsScrmPulpSdk extends SdkBase
+{
+    /**
+     * 服务名称
+     * 自来`postman.json`文件定义的`sdkService`值
+     * @var string
+     */
+    protected $serviceName = 'ps-scrm-pulp';
+
+    /**
+     * 新增
+     * @link https://uniondrug.coding.net/p/ps-scrm-pulp/git/blob/development/docs/api/MemberController/createAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function memberCreate($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/member/create", $body, $query, $extra);
+    }
+
+    /**
+     * 详情
+     * @link https://uniondrug.coding.net/p/ps-scrm-pulp/git/blob/development/docs/api/MemberController/detailAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function memberDetail($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/member/detail", $body, $query, $extra);
+    }
+
+    /**
+     * 分页列表
+     * @link https://uniondrug.coding.net/p/ps-scrm-pulp/git/blob/development/docs/api/MemberController/pagingAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function memberPaging($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/member/paging", $body, $query, $extra);
+    }
+
+    /**
+     * 编辑
+     * @link https://uniondrug.coding.net/p/ps-scrm-pulp/git/blob/development/docs/api/MemberController/updateAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function memberUpdate($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/member/update", $body, $query, $extra);
+    }
+}
