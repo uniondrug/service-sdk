03076b3cd87911b90f4704d690ebd8744ee85008
chenwei <chenwei@uniondrug.cn>
2021-08-13 14:39:47 +0800

    psScrmPulp

--- src/Exports/Modules/PsScrmPulpSdk.php
+++ src/Exports/Modules/PsScrmPulpSdk.php
@@ -9,7 +9,7 @@
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
  * @date   2021-08-13
- * @time   Fri, 13 Aug 2021 14:25:46 +0800
+ * @time   Fri, 13 Aug 2021 14:37:56 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
@@ -144,32 +144,6 @@ class PsScrmPulpSdk extends SdkBase
     public function praiseInfo($body, $query = null, $extra = null)
     {
         return $this->restful("POST", "/praise/info", $body, $query, $extra);
-    }
-
-    /**
-     * 修改
-     * @link https://uniondrug.coding.net/p/ps-scrm-pulp/git/blob/development/docs/api/ReedomCodeController/updateAction.md
-     * @param array|object $body 入参类型
-     * @param null $query  Query数据
-     * @param null $extra  请求头信息
-     * @return ResponseInterface
-     */
-    public function reedomCodeUpdate($body, $query = null, $extra = null)
-    {
-        return $this->restful("POST", "/reedomCode/update", $body, $query, $extra);
-    }
-
-    /**
-     * 分页列表
-     * @link https://uniondrug.coding.net/p/ps-scrm-pulp/git/blob/development/docs/api/SendRecordController/pageAction.md
-     * @param array|object $body 入参类型
-     * @param null $query  Query数据
-     * @param null $extra  请求头信息
-     * @return ResponseInterface
-     */
-    public function sendRecordPage($body, $query = null, $extra = null)
-    {
-        return $this->restful("POST", "/sendRecord/page", $body, $query, $extra);
     }
 
     /**
