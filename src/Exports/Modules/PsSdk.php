11aa8044fcfbe5a0ac64450b0d4b8f68561d75ec
康小强 <kangxiaoqiang@hoge.cn>
2021-08-16 14:40:36 +0800

    ps-bsdata

--- src/Exports/Modules/PsSdk.php
+++ src/Exports/Modules/PsSdk.php
@@ -860,4 +860,17 @@ class PsSdk extends SdkBase
     {
         return $this->restful("POST", "/userMerchantM/getByMerchantId", $body, $query, $extra);
     }
+
+    /**
+     * 商户列表导出
+     * @link https://uniondrug.coding.net/p/bsdata.ps/git/blob/development/docs/api/PartnerStatisticController/exportAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function partnerStatisticExport($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/partnerStatistic/export", $body, $query, $extra);
+    }
 }
