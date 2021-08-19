15b92476a66296816a870e5102009f6b3133d8a3
liuhaifeng <liuhaifeng@uniondrug.com>
2021-08-19 13:48:01 +0800

    财务sdk

--- src/Exports/Modules/SettlementSdk.php
+++ src/Exports/Modules/SettlementSdk.php
@@ -5112,4 +5112,17 @@ class SettlementSdk extends SdkBase
     {
         return $this->restful("POST", "/unitBlockInfo/update", $body, $query, $extra);
     }
+    
+    /**
+     * 批量获取开票单明细
+     * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/BillController/getStatementNoByBillNosAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function billGetBatchStatementNos($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/bill/get/batch/statementNos", $body, $query, $extra);
+    }
 }
