49570e7ff7becafec2d6adb3a000f28ad321e2c6
liuhaifeng <liuhaifeng@uniondrug.com>
2021-08-26 09:48:01 +0800

    财务Sdk

--- src/Exports/Modules/JmInsureSdk.php
+++ src/Exports/Modules/JmInsureSdk.php
@@ -1206,4 +1206,17 @@ class JmInsureSdk extends SdkBase
     {
         return $this->restful("POST", "/ipackageOrder/reportBillPage", $body);
     }
+
+    /**
+     * 分页查询核算单位下赔案回款 保司集合
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/feature_3.12/doc/api/IpackageOrderController/reportInsurerPage.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function reportInsurerPage($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/ipackageOrder/reportInsurerPage", $body);
+    }
 }
