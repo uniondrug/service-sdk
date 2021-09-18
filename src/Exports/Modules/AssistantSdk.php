87574aab7729c1c01d2f3b693d47496d1457694e
康小强 <kangxiaoqiang@hoge.cn>
2021-09-18 14:47:19 +0800

    zhushou

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -5299,4 +5299,17 @@ class AssistantSdk extends SdkBase
     {
         return $this->restful("POST", "/insuranceStatistic/viewAuthorityEdit", $body, $query, $extra);
     }
+
+    /**
+     * 审批流模板 [添加]
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Approval/TemplateApproveController/addAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function templateAdd($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/template/add", $body, $query, $extra);
+    }
 }
