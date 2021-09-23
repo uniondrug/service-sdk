14b97ac7b8d1c9465ef4bbd7783b2eb9b35a7d0c
william-wqq <william_wqq@163.com>
2021-09-23 15:02:05 +0800

    新增ClerkSdk接口

--- src/Exports/Modules/ClerkSdk.php
+++ src/Exports/Modules/ClerkSdk.php
@@ -368,6 +368,19 @@ class ClerkSdk extends SdkBase
     }
 
     /**
+     * 店员退款风控
+     * @link https://uniondrug.coding.net/p/module.clerk/git/blob/development/docs/api/AssistantController/assistantRefundRiskManageAction.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function assistantRefundRiskManage($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/assistant/assistantRefundRiskManage", $body, $query, $extra);
+    }
+
+    /**
      * 店员pos关闭
      * @link https://uniondrug.coding.net/p/module.clerk/git/blob/development/docs/api/AssistantController/posDisableAction.md
      * @param array|object $body  入参类型
