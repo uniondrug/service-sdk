4fefadd85a19d3ffc8a483bb186aa6f4b29c10a9
Mario Zhao <shuo_128@foxmail.com>
2021-10-15 16:19:51 +0800

    吐槽

--- src/Exports/Modules/PsFinanceDataSdk.php
+++ src/Exports/Modules/PsFinanceDataSdk.php
@@ -2333,4 +2333,16 @@ class PsFinanceDataSdk extends SdkBase
     {
         return $this->restful("POST", "/organize/qualification/check", $body, $query, $extra);
     }
+
+    /**
+     * 吐槽
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function feedbackCreate($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/feedback/create", $body, $query, $extra);
+    }
 }
