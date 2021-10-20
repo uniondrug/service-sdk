025aa252b110560b23984ee1bca272f86575f652
Haven <chenhao@uniondrug.com>
2021-10-20 13:45:09 +0800

    Update JmInsureSdk.php
    理赔统计接口

--- src/Exports/Modules/JmInsureSdk.php
+++ src/Exports/Modules/JmInsureSdk.php
@@ -255,6 +255,45 @@ class JmInsureSdk extends SdkBase
     }
 
     /**
+     * 保司理赔情况分页查询
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/ClaimController/page.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function claimStatisticsInsurerPage($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/claim/statistics/insurer/page", $body);
+    }
+
+    /**
+     * 保单理赔情况分页查询
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/ClaimController/page.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function claimStatisticsPolicyPage($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/claim/statistics/policy/page", $body);
+    }
+
+    /**
+     * 理赔单理赔情况分页查询
+     * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/ClaimController/page.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function claimStatisticsIpackagePage($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/claim/statistics/ipackage/page", $body);
+    }
+
+    /**
      * 理赔单关联订单分页查询
      * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/development/doc/api/IpackageOrderController/page.md
      * @param array|object $body  入参类型
@@ -1180,7 +1219,7 @@ class JmInsureSdk extends SdkBase
     {
         return $this->restful("POST", "/insure/pageCompany", $body);
     }
-    
+
     /**
      * 分页查询核算单位下赔案回款
      * @link https://git.uniondrug.com/code/1/finance/shares/insure3/blob/feature_14212/doc/api/IpackageOrderController/reportPage.md
