3e53b7472b65bc2a052b890cfc81d9b91bdc4db0
Mario Zhao <shuo_128@foxmail.com>
2021-10-21 17:45:30 +0800

    健康服务结算

--- src/Exports/Modules/GsFinStatementSdk.php
+++ src/Exports/Modules/GsFinStatementSdk.php
@@ -249,7 +249,7 @@ class GsFinStatementSdk extends SdkBase
     }
 
     /**
-     * 健康服务结算-开票销货清单生成
+     * 健康服务结算-健康服务结算手动申请开票
      * @param $body
      * @return ResponseInterface
      */
@@ -257,5 +257,74 @@ class GsFinStatementSdk extends SdkBase
     {
         return $this->restful("POST", "/healthy/bill/apply", $body);
     }
-}
 
+    /**
+     * 健康服务结算-健康服务结算创建付款单
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function healthyStatementPaymentCreate($body)
+    {
+        return $this->restful("POST", "/healthy/payment/create", $body);
+    }
+
+    /**
+     * 换新结算-发起
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function renewStatementCreate($body)
+    {
+        return $this->restful("POST", "/renew/statement/create", $body);
+    }
+
+    /**
+     * 换新结算-付款单列表
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function renewStatementPaymentList($body)
+    {
+        return $this->restful("POST", "/renew/payment/list", $body);
+    }
+
+    /**
+     * 换新结算-账单汇总
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function renewStatementSummaryList($body)
+    {
+        return $this->restful("POST", "/renew/summary/list", $body);
+    }
+
+    /**
+     * 换新结算-详情
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function renewStatementDetail($body)
+    {
+        return $this->restful("POST", "/renew/statement/detail", $body);
+    }
+
+    /**
+     * 换新结算-审核通过
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function renewStatementAuditAccept($body)
+    {
+        return $this->restful("POST", "/renew/statement/audit/accept", $body);
+    }
+
+    /**
+     * 换新结算-审核拒绝
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function renewStatementAuditRefuse($body)
+    {
+        return $this->restful("POST", "/renew/statement/audit/refuse", $body);
+    }
+}
