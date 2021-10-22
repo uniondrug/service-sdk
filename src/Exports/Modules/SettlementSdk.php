304e9520450bfc792c4451fa86dba889534c2d49
liuhaifeng <liuhaifeng@uniondrug.com>
2021-10-22 10:30:16 +0800

    财务sdk

--- src/Exports/Modules/SettlementSdk.php
+++ src/Exports/Modules/SettlementSdk.php
@@ -5217,4 +5217,17 @@ class SettlementSdk extends SdkBase
     {
         return $this->restful("POST", "/bill/insure/company/list", $body, $query, $extra);
     }
+
+    /**
+     * 根据结算单获取已生成开票单金额
+     * @link https://uniondrug.coding.net/p/module.settlement/git/blob/development/docs/api/PaymentController/manualCreateAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function billAmountByStatementNo($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/bill/amount/by/statementNo", $body, $query, $extra);
+    }
 }
