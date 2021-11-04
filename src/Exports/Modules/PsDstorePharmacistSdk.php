047fa515e2b2e8789fc8830b92638066533a4488
xujing <xujing@xujingdeMacBook-Pro.local>
2021-11-04 16:58:20 +0800

    PsDstorePharmacistSdk

--- src/Exports/Modules/PsDstorePharmacistSdk.php
+++ src/Exports/Modules/PsDstorePharmacistSdk.php
@@ -9,7 +9,7 @@
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
  * @date   2021-11-04
- * @time   Thu, 04 Nov 2021 16:33:08 +0800
+ * @time   Thu, 04 Nov 2021 16:56:56 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
@@ -40,6 +40,19 @@ class PsDstorePharmacistSdk extends SdkBase
     public function getProjectByType($body, $query = null, $extra = null)
     {
         return $this->restful("POST", "/project/getProjectByType", $body, $query, $extra);
+    }
+
+    /**
+     * 获取项目配置
+     * @link https://uniondrug.coding.net/p/ps-dstore-pharmacist/git/blob/development/docs/api/ProjectController/getProjectConfigAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function getProjectConfig($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/project/getProjectConfig", $body, $query, $extra);
     }
 
     /**
