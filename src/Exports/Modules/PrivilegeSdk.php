a476346cc5c411a0643cda0e03e1adb3d69c4aff
liyang <952154830@qq.com>
2021-11-22 17:32:56 +0800

    psdstoremember

--- src/Exports/Modules/PrivilegeSdk.php
+++ src/Exports/Modules/PrivilegeSdk.php
@@ -8,8 +8,8 @@
  * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
- * @date   2021-10-25
- * @time   Mon, 25 Oct 2021 13:29:45 +0800
+ * @date   2021-11-22
+ * @time   Mon, 22 Nov 2021 17:31:44 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
@@ -287,6 +287,19 @@ class PrivilegeSdk extends SdkBase
     public function privilegeManagerPaging($body, $query = null, $extra = null)
     {
         return $this->restful("POST", "/privilege/manager/paging", $body, $query, $extra);
+    }
+
+    /**
+     * 特权规则编辑
+     * @link https://uniondrug.coding.net/p/module.privilege/git/blob/development/docs/api/Privilege/PrivilegeController/orderDetailAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function privilegeOrderDetail($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/privilege/order/detail", $body, $query, $extra);
     }
 
     /**
