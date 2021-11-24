a21b1c90aea4a4a548cf645983e9f081d45b3697
Haven <chenhao@uniondrug.com>
2021-11-24 17:36:14 +0800

    Update PsFinanceDataSdk.php
    关闭结算借口

--- src/Exports/Modules/PsFinanceDataSdk.php
+++ src/Exports/Modules/PsFinanceDataSdk.php
@@ -602,6 +602,58 @@ class PsFinanceDataSdk extends SdkBase
     }
 
     /**
+     * 新增
+     * @link https://uniondrug.coding.net/p/ps-finance-data/git/blob/development/docs/api/Partner/AuthorityController/createAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function partnerAuthorityCreate($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/partner/authority/create", $body, $query, $extra);
+    }
+
+    /**
+     * 商家详情
+     * @link https://uniondrug.coding.net/p/ps-finance-data/git/blob/development/docs/api/Partner/AuthorityController/merchantDetailAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function partnerAuthorityMerchantDetail($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/partner/authority/merchant/detail", $body, $query, $extra);
+    }
+
+    /**
+     * 分页列表
+     * @link https://uniondrug.coding.net/p/ps-finance-data/git/blob/development/docs/api/Partner/AuthorityController/pagingAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function partnerAuthorityPaging($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/partner/authority/paging", $body, $query, $extra);
+    }
+
+    /**
+     * 修改
+     * @link https://uniondrug.coding.net/p/ps-finance-data/git/blob/development/docs/api/Partner/AuthorityController/updateStatusAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function partnerAuthorityUpdateStatus($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/partner/authority/update/status", $body, $query, $extra);
+    }
+
+    /**
      * 连锁开票待审核列表
      * @link https://uniondrug.coding.net/p/ps-finance-data/git/blob/development/docs/api/Partner/BillInfoController/auditListingAction.md
      * @param array|object $body 入参类型
