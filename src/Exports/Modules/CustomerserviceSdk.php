e41bdaecfd356c5498ab53a8b0939a7f0ef70e99
mengqing <mengqing@uniondrug.com>
2021-11-30 11:12:30 +0800

    更新客服中心 sdk

--- src/Exports/Modules/CustomerserviceSdk.php
+++ src/Exports/Modules/CustomerserviceSdk.php
@@ -8,8 +8,8 @@
  * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
- * @date   2020-05-22
- * @time   Fri, 22 May 2020 14:13:51 +0800
+ * @date   2021-11-30
+ * @time   Tue, 30 Nov 2021 11:09:26 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
@@ -32,199 +32,53 @@ class CustomerserviceSdk extends SdkBase
     /**
      * 投诉记录统计
      * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/ComplainController/complainCountAction.md
-     * @param array $body 入参类型
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
      * @return ResponseInterface
      */
-    public function complainCount($body)
+    public function complainCount($body, $query = null, $extra = null)
     {
-        return $this->restful("POST", "/complain/count", $body);
+        return $this->restful("POST", "/complain/count", $body, $query, $extra);
     }
 
     /**
      * 添加投诉记录列表
      * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/ComplainController/complainCreatAction.md
-     * @param array $body 入参类型
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
      * @return ResponseInterface
      */
-    public function complainCreat($body)
+    public function complainCreat($body, $query = null, $extra = null)
     {
-        return $this->restful("POST", "/complain/Creat", $body);
+        return $this->restful("POST", "/complain/Creat", $body, $query, $extra);
     }
 
     /**
      * 投诉记录列表
      * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/ComplainController/complainPagingAction.md
-     * @param array $body 入参类型
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
      * @return ResponseInterface
      */
-    public function complainPaging($body)
+    public function complainPaging($body, $query = null, $extra = null)
     {
-        return $this->restful("POST", "/complain/paging", $body);
+        return $this->restful("POST", "/complain/paging", $body, $query, $extra);
     }
 
     /**
      * 添加操作日志
      * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/OperateLogController/createAction.md
-     * @param array $body 入参类型
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
      * @return ResponseInterface
      */
-    public function createOperateLog($body)
+    public function createOperateLog($body, $query = null, $extra = null)
     {
-        return $this->restful("POST", "/operate/log/create", $body);
-    }
-
-    /**
-     * 读取操作日志
-     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/OperateLogController/listAction.md
-     * @param array $body 入参类型
-     * @return ResponseInterface
-     */
-    public function listOperateLog($body)
-    {
-        return $this->restful("POST", "/operate/log/list", $body);
-    }
-
-    /**
-     * 新增
-     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/LogMsgController/createAction.md
-     * @param array $body 入参类型
-     * @return ResponseInterface
-     */
-    public function logMsgCreate($body)
-    {
-        return $this->restful("POST", "/logMsg/create", $body);
-    }
-
-    /**
-     * 删除
-     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/LogMsgController/deleteAction.md
-     * @param array $body 入参类型
-     * @return ResponseInterface
-     */
-    public function logMsgDelete($body)
-    {
-        return $this->restful("POST", "/logMsg/delete", $body);
-    }
-
-    /**
-     * 详情
-     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/LogMsgController/detailAction.md
-     * @param array $body 入参类型
-     * @return ResponseInterface
-     */
-    public function logMsgDetail($body)
-    {
-        return $this->restful("POST", "/logMsg/detail", $body);
-    }
-
-    /**
-     * 无分页列表
-     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/LogMsgController/listingAction.md
-     * @param array $body 入参类型
-     * @return ResponseInterface
-     */
-    public function logMsgListing($body)
-    {
-        return $this->restful("POST", "/logMsg/listing", $body);
-    }
-
-    /**
-     * 分页列表
-     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/LogMsgController/pagingAction.md
-     * @param array $body 入参类型
-     * @return ResponseInterface
-     */
-    public function logMsgPaging($body)
-    {
-        return $this->restful("POST", "/logMsg/paging", $body);
-    }
-
-    /**
-     * 修改
-     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/LogMsgController/updateAction.md
-     * @param array $body 入参类型
-     * @return ResponseInterface
-     */
-    public function logMsgUpdate($body)
-    {
-        return $this->restful("POST", "/logMsg/update", $body);
-    }
-
-    /**
-     * 退款申请记录添加
-     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/Refund/ApplyController/createAction.md
-     * @param array $body 入参类型
-     * @return ResponseInterface
-     */
-    public function orderRefundApplyCreate($body)
-    {
-        return $this->restful("POST", "/refundapply/create", $body);
-    }
-
-    /**
-     * 退款申请单状态变更-拒绝
-     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/Refund/ApplyController/disableAction.md
-     * @param array $body 入参类型
-     * @return ResponseInterface
-     */
-    public function orderRefundApplyDisable($body)
-    {
-        return $this->restful("POST", "/refundapply/disable", $body);
-    }
-
-    /**
-     * 退款申请单记录详情
-     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/Refund/ApplyController/infoAction.md
-     * @param array $body 入参类型
-     * @return ResponseInterface
-     */
-    public function orderRefundApplyInfo($body)
-    {
-        return $this->restful("POST", "/refundapply/info", $body);
-    }
-
-    /**
-     * 退款申请记录列表-分页
-     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/Refund/ApplyController/pagingAction.md
-     * @param array $body 入参类型
-     * @return ResponseInterface
-     */
-    public function orderRefundApplyPaging($body)
-    {
-        return $this->restful("POST", "/refundapply/paging", $body);
-    }
-
-    /**
-     * 退款申请单备注
-     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/Refund/ApplyController/remarkAction.md
-     * @param array $body 入参类型
-     * @return ResponseInterface
-     */
-    public function orderRefundApplyRemark($body)
-    {
-        return $this->restful("POST", "/refundapply/remark", $body);
-    }
-
-    /**
-     * 退款申请单状态变更-已退款
-     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/Refund/ApplyController/successAction.md
-     * @param array $body 入参类型
-     * @return ResponseInterface
-     */
-    public function orderRefundApplySuccess($body)
-    {
-        return $this->restful("POST", "/refundapply/success", $body);
-    }
-
-    /**
-     * 详情
-     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/UserJobNumberController/detailAction.md
-     * @param array $body 入参类型
-     * @return ResponseInterface
-     */
-    public function userJobNumberDetail($body)
-    {
-        return $this->restful("POST", "/userJobNumber/detail", $body);
+        return $this->restful("POST", "/operate/log/create", $body, $query, $extra);
     }
 
     /**
@@ -238,6 +92,214 @@ class CustomerserviceSdk extends SdkBase
     public function createWorkOrder($body, $query = null, $extra = null)
     {
         return $this->restful("POST", "/workorder/create", $body, $query, $extra);
+    }
+
+    /**
+     * 读取操作日志
+     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/OperateLogController/listAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function listOperateLog($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/operate/log/list", $body, $query, $extra);
+    }
+
+    /**
+     * 新增
+     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/LogMsgController/createAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function logMsgCreate($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/logMsg/create", $body, $query, $extra);
+    }
+
+    /**
+     * 删除
+     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/LogMsgController/deleteAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function logMsgDelete($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/logMsg/delete", $body, $query, $extra);
+    }
+
+    /**
+     * 详情
+     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/LogMsgController/detailAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function logMsgDetail($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/logMsg/detail", $body, $query, $extra);
+    }
+
+    /**
+     * 无分页列表
+     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/LogMsgController/listingAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function logMsgListing($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/logMsg/listing", $body, $query, $extra);
+    }
+
+    /**
+     * 分页列表
+     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/LogMsgController/pagingAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function logMsgPaging($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/logMsg/paging", $body, $query, $extra);
+    }
+
+    /**
+     * 修改
+     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/LogMsgController/updateAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function logMsgUpdate($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/logMsg/update", $body, $query, $extra);
+    }
+
+    /**
+     * 操作日志分页
+     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/OperateLogController/pageAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function operateLogPage($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/operate/log/page", $body, $query, $extra);
+    }
+
+    /**
+     * 退款申请单状态变更-已撤销
+     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/Refund/ApplyController/cancelAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function orderRefundApplyCancel($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/refundapply/cancel", $body, $query, $extra);
+    }
+
+    /**
+     * 退款申请记录添加
+     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/Refund/ApplyController/createAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function orderRefundApplyCreate($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/refundapply/create", $body, $query, $extra);
+    }
+
+    /**
+     * 退款申请单状态变更-拒绝
+     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/Refund/ApplyController/disableAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function orderRefundApplyDisable($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/refundapply/disable", $body, $query, $extra);
+    }
+
+    /**
+     * 退款申请单记录详情
+     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/Refund/ApplyController/infoAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function orderRefundApplyInfo($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/refundapply/info", $body, $query, $extra);
+    }
+
+    /**
+     * 退款申请记录列表-分页
+     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/Refund/ApplyController/pagingAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function orderRefundApplyPaging($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/refundapply/paging", $body, $query, $extra);
+    }
+
+    /**
+     * 退款申请单备注
+     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/Refund/ApplyController/remarkAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function orderRefundApplyRemark($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/refundapply/remark", $body, $query, $extra);
+    }
+
+    /**
+     * 退款申请单状态变更-已退款
+     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/Refund/ApplyController/successAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function orderRefundApplySuccess($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/refundapply/success", $body, $query, $extra);
+    }
+
+    /**
+     * 详情
+     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/UserJobNumberController/detailAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function userJobNumberDetail($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/userJobNumber/detail", $body, $query, $extra);
     }
 
     /**
@@ -290,18 +352,5 @@ class CustomerserviceSdk extends SdkBase
     public function workOrderRecall($body, $query = null, $extra = null)
     {
         return $this->restful("POST", "/workorder/recall", $body, $query, $extra);
-    }
-
-    /**
-     * 操作日志分页
-     * @link https://uniondrug.coding.net/p/module.customerservice/git/blob/development/docs/api/OperateLogController/pageAction.md
-     * @param array|object $body 入参类型
-     * @param null $query  Query数据
-     * @param null $extra  请求头信息
-     * @return ResponseInterface
-     */
-    public function operateLogPage($body, $query = null, $extra = null)
-    {
-        return $this->restful("POST", "/operate/log/page", $body, $query, $extra);
     }
 }
