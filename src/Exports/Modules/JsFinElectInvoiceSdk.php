a03e13ebe20a59cc7b717633dc552908bfbfaa28
Mario Zhao <shuo_128@foxmail.com>
2021-12-07 20:57:13 +0800

    查询服务明细财务共享服务平台

--- src/Exports/Modules/JsFinElectInvoiceSdk.php
+++ src/Exports/Modules/JsFinElectInvoiceSdk.php
@@ -23,6 +23,7 @@ class JsFinElectInvoiceSdk extends SdkBase
 
     /**
      * 诺E任务回调接口
+     * @link https://git.uniondrug.com/code/1/finance/others/js-fin-elec-invoice/blob/development/doc/api/CallBackController/receviceNuoEInvoice.md
      * @param $body
      * @return ResponseInterface
      */
@@ -33,11 +34,89 @@ class JsFinElectInvoiceSdk extends SdkBase
 
     /**
      * 诺诺任务回调接口
+     * @link https://git.uniondrug.com/code/1/finance/others/js-fin-elec-invoice/blob/development/doc/api/CallBackController/receiveNuoNuoInvoice.md
      * @param $body
      * @return ResponseInterface
      */
     public function callbackNuoNuo($body)
     {
         return $this->restful("POST", "/callback/nuonuo/receiveInvoice", $body);
+    }
+
+    /**
+     * 查询当前药联们公司的库存
+     * @link https://git.uniondrug.com/code/1/finance/others/js-fin-elec-invoice/blob/development/doc/api/UniondrugCompanyController/getInvoiceStock.md
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function getInvoiceStock($body)
+    {
+        return $this->restful("POST", "/company/getInvoiceStock", $body);
+    }
+
+    /**
+     * 查询开票主体列表
+     * @link https://git.uniondrug.com/code/1/finance/others/js-fin-elec-invoice/blob/development/doc/api/UniondrugCompanyController/pageInvoiceEntity.md
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function invoiceEntityPaging($body)
+    {
+        return $this->restful("POST", "/company/pageInvoiceEntity", $body);
+    }
+
+    /**
+     * 发送邮件(通过业务的单号)
+     * @link https://git.uniondrug.com/code/1/finance/others/js-fin-elec-invoice/blob/development/doc/api/OperationController/sendEmail.md
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function sendEmail($body)
+    {
+        return $this->restful("POST", "/mbs/operate/sendEmail", $body);
+    }
+
+    /**
+     * 盖章
+     * @link https://git.uniondrug.com/code/1/finance/others/js-fin-elec-invoice/blob/development/doc/api/OperationController/sign.md
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function sign($body)
+    {
+        return $this->restful("POST", "/mbs/operate/sign", $body);
+    }
+
+    /**
+     * 合并PDF
+     * @link https://git.uniondrug.com/code/1/finance/others/js-fin-elec-invoice/blob/development/doc/api/OperationController/mergePdf.md
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function mergePDF($body)
+    {
+        return $this->restful("POST", "/mbs/operate/mergePdf", $body);
+    }
+
+    /**
+     * 生成服务明细
+     * @link https://git.uniondrug.com/code/1/finance/others/js-fin-elec-invoice/blob/development/doc/api/OperationController/createItem.md
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function orderItemCreate($body)
+    {
+        return $this->restful("POST", "/mbs/operate/createItem", $body);
+    }
+
+    /**
+     * 查询服务明细财务共享服务平台-c端电子发票-详情-服务明细
+     * @link https://git.uniondrug.com/code/1/finance/others/js-fin-elec-invoice/blob/development/doc/api/BillTaskInfoController/pageServiceDetail.md
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function serviceDetailPaging($body)
+    {
+        return $this->restful("POST", "/billtaskInfo/pageServiceDetail", $body);
     }
 }
