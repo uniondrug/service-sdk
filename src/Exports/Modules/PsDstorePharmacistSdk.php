63f5d8d9047c54dc75d99fa6ce4ade00c9819336
xujing <xujing@xujingdeMacBook-Pro.local>
2021-12-08 17:29:41 +0800

    PsDstoreMemberSdk

--- src/Exports/Modules/PsDstorePharmacistSdk.php
+++ src/Exports/Modules/PsDstorePharmacistSdk.php
@@ -8,8 +8,8 @@
  * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
- * @date   2021-12-07
- * @time   Tue, 07 Dec 2021 16:00:16 +0800
+ * @date   2021-12-08
+ * @time   Wed, 08 Dec 2021 17:27:51 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
@@ -28,6 +28,19 @@ class PsDstorePharmacistSdk extends SdkBase
      * @var string
      */
     protected $serviceName = 'ps-dstore-pharmacist';
+
+    /**
+     * 获取用户和药师最近一次的会话
+     * @link https://uniondrug.coding.net/p/ps-dstore-pharmacist/git/blob/development/docs/api/ConversationController/getLastConversationAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function getLastConversation($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/conversation/getLastConversation", $body, $query, $extra);
+    }
 
     /**
      * 获取项目配置的药师列表页宣传图
