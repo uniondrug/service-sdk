15732b68d737ff041ae66d2ebe811fa3836cf3ba
liyang <952154830@qq.com>
2021-12-08 10:45:50 +0800

    psdstoremember

--- src/Exports/Modules/PrivilegeSdk.php
+++ src/Exports/Modules/PrivilegeSdk.php
@@ -8,8 +8,8 @@
  * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
- * @date   2021-11-22
- * @time   Mon, 22 Nov 2021 17:31:44 +0800
+ * @date   2021-12-08
+ * @time   Wed, 08 Dec 2021 10:44:18 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
@@ -131,6 +131,19 @@ class PrivilegeSdk extends SdkBase
     public function privilegeCheckOrgan($body, $query = null, $extra = null)
     {
         return $this->restful("POST", "/privilege/check/organ", $body, $query, $extra);
+    }
+
+    /**
+     * 特权规则编辑
+     * @link https://uniondrug.coding.net/p/module.privilege/git/blob/development/docs/api/Privilege/PrivilegeController/copyTaskAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function privilegeCopyTask($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/privilege/copy/task", $body, $query, $extra);
     }
 
     /**
