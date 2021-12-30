5491c2ab9b85c1cf1795b7836e79dd8039706ac0
Darrow <346300265@qq.com>
2021-12-30 09:48:56 +0800

    添加保障新项目SDK

---
+++ src/Exports/Modules/PsGuaranteeSdk.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * 重要说明
+ * 1. 本文件由Postman命令脚本自动生成, 请不要修改, 若需修改
+ *    请通过`php console postman`命令重新生成.
+ * 2. 本脚本在生成时, 依赖所在项目的Controller有 `@Sdk method`定义,
+ *    同时, 项目根目录下的`postman.json`需有`sdk`、`sdkLink`定义
+ * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
+ *    的`src/Exports/Modules`目录下，并发重新发布release版本.
+ * @author PostmanCommand
+ * @date   2021-12-29
+ * @time   Wed, 29 Dec 2021 15:42:14 +0800
+ */
+namespace Uniondrug\ServiceSdk\Exports\Modules;
+
+use Uniondrug\ServiceSdk\Exports\Abstracts\SdkBase;
+use Uniondrug\ServiceSdk\Bases\ResponseInterface;
+
+/**
+ * PsGuaranteeSdk
+ * @package Uniondrug\ServiceSdk\Modules
+ */
+class PsGuaranteeSdk extends SdkBase
+{
+    /**
+     * 服务名称
+     * 自来`postman.json`文件定义的`sdkService`值
+     * @var string
+     */
+    protected $serviceName = 'ps-guarantee';
+
+
+    /**
+     * 激活保障
+     * @link https://uniondrug.coding.net/p/ps-guarantee/git/tree/development/docs/api/GuaranteeController/activeAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function ActiveGuarantee($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/guarantee/active", $body, $query, $extra);
+    }
+
+}
