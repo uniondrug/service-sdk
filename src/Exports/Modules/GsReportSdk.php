cea4f2086a55b388abc25f0ff805661f691b58dd
Haven <chenhao@uniondrug.com>
2022-01-12 14:14:38 +0800

    数据报表服务接口

---
+++ src/Exports/Modules/GsReportSdk.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * 重要说明
+ * 1. 本文件由Postman命令脚本自动生成, 请不要修改, 若需修改
+ *    请通过`php console postman`命令重新生成.
+ * 2. 本脚本在生成时, 依赖所在项目的Controller有 `@Sdk method`定义,
+ *    同时, 项目根目录下的`postman.json`需有`sdk`、`sdkLink`定义
+ * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
+ *    的`src/Exports/Modules`目录下，并发重新发布release版本.
+ * @author PostmanCommand
+ * @date   2022-01-12
+ * @time   Thu, 02 Apr 2020 01:00:11 +0800
+ */
+namespace Uniondrug\ServiceSdk\Exports\Modules;
+
+use Uniondrug\ServiceSdk\Exports\Abstracts\SdkBase;
+use Uniondrug\ServiceSdk\Bases\ResponseInterface;
+
+/**
+ * Class GsReportSdk
+ * @package Uniondrug\ServiceSdk\Exports\Modules
+ */
+class GsReportSdk extends SdkBase
+{
+    /**
+     * 服务名称
+     * @var string
+     */
+    protected $serviceName = 'gs-report';
+
+    /**
+     * 报表列表
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function reportList($body)
+    {
+        return $this->restful("GET", "/report/list", $body);
+    }
+}
