47b08af9c39157ec5511b9c366ed19de5acf9a67
mengqing <mengqing@uniondrug.com>
2022-02-17 17:54:25 +0800

    更新 sdk

--- src/Exports/Modules/StagnationSdk.php
+++ src/Exports/Modules/StagnationSdk.php
@@ -8,8 +8,8 @@
  * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
- * @date   2021-11-16
- * @time   Tue, 16 Nov 2021 11:44:47 +0800
+ * @date   2022-02-17
+ * @time   Thu, 17 Feb 2022 17:52:19 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
@@ -368,6 +368,19 @@ class StagnationSdk extends SdkBase
     public function groupManageOccupy($body, $query = null, $extra = null)
     {
         return $this->restful("POST", "/group/manage/occupy", $body, $query, $extra);
+    }
+
+    /**
+     * 变更驻店宝分组状态
+     * @link https://uniondrug.coding.net/p/module.stagnation/git/blob/development/docs/api/GroupManageController/statusAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function groupManageStatus($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/group/manage/status", $body, $query, $extra);
     }
 
     /**
