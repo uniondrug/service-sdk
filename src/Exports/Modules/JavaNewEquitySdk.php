9d129c3e7dca38022fb47b1e230cf88186c63df0
mengqing <mengqing@uniondrug.com>
2022-03-04 10:24:08 +0800

    更新 sdk

--- src/Exports/Modules/JavaNewEquitySdk.php
+++ src/Exports/Modules/JavaNewEquitySdk.php
@@ -235,4 +235,15 @@ class JavaNewEquitySdk extends SdkBase
     {
         return $this->restful("POST", "/agrmng/mng/code/createCode", $body);
     }
+
+    /**
+     * 回收兑换码
+     * @param array $body 入参类型
+     * @return ResponseInterface
+     * @link https://git.uniondrug.com/docs/1/docs-bizcenter/php-docs/blob/master/js.esop.parent/api/js-esop-agreement-svr/CodeSvrController/recycleCode.md
+     */
+    public function agrsrvCodeRecycle($body)
+    {
+        return $this->restful("POST", "/agrsrv/srv/code/recycle", $body);
+    }
 }
