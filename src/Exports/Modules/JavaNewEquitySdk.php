67ba23730ab377c24adf70ff666a039e04f678b7
mengqing <mengqing@uniondrug.com>
2022-03-09 15:00:43 +0800

    更新 sdk

--- src/Exports/Modules/JavaNewEquitySdk.php
+++ src/Exports/Modules/JavaNewEquitySdk.php
@@ -244,6 +244,6 @@ class JavaNewEquitySdk extends SdkBase
      */
     public function agrsrvCodeRecycle($body)
     {
-        return $this->restful("POST", "/agrsrv/srv/code/recycle", $body);
+        return $this->restful("POST", "/agrsvr/srv/code/recycle", $body);
     }
 }
