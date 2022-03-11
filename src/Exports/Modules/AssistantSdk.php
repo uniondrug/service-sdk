e83ae8539a223d751a7680c71326e43ccec8eff5
康小强 <kangxiaoqiang@hoge.cn>
2022-03-11 14:56:01 +0800

    zhushou

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -5356,12 +5356,38 @@ class AssistantSdk extends SdkBase
      * 绩效审批列表
      * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Statistic/Insurance/KpiStatisticController/applyListAction.md
      * @param array|object $body 入参类型
-     * @param null $query  Query数据
-     * @param null $extra  请求头信息
+     * @param null $query Query数据
+     * @param null $extra 请求头信息
      * @return ResponseInterface
      */
     public function kpiApplyList($body, $query = null, $extra = null)
     {
         return $this->restful("POST", "/kpiStatistic/apply/list", $body, $query, $extra);
+    }
+
+    /**
+     * 绩效审批提交
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Statistic/Insurance/KpiStatisticController/applyAddAction.md
+     * @param array|object $body 入参类型
+     * @param null $query Query数据
+     * @param null $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function kpiApplyAdd($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/kpiStatistic/apply/add", $body, $query, $extra);
+    }
+
+    /**
+     * 反馈提交
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Statistic/Insurance/KpiStatisticController/feedbackAddAction.md
+     * @param array|object $body 入参类型
+     * @param null $query Query数据
+     * @param null $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function kpiFeedbackAdd($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/kpiStatistic/feedback/add", $body, $query, $extra);
     }
 }
