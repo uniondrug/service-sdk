78d95b1751a66c043652a30906a6f2396df8b37f
mengqing <mengqing@uniondrug.com>
2022-03-16 17:17:28 +0800

    更新 sdk

--- src/Exports/Modules/AssistantSdk.php
+++ src/Exports/Modules/AssistantSdk.php
@@ -5464,4 +5464,17 @@ class AssistantSdk extends SdkBase
     {
         return $this->restful("POST", "/kpiStatistic/need/complete/ratio", $body, $query, $extra);
     }
+
+    /**
+     * 保司高层拜访分页
+     * @link https://uniondrug.coding.net/p/module.assistant/git/tree/development/docs/api/Statistic/Insurance/KpiStatisticController/visitPageAction.md
+     * @param array|object $body  入参类型
+     * @param null         $query Query数据
+     * @param null         $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function kpiVisitPage($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/kpiStatistic/visit/page", $body, $query, $extra);
+    }
 }
