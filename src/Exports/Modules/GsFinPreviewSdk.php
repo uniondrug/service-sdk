93a478cea22afe8b5e15b88d71d4fca441c3b14d
Haven <chenhao@uniondrug.com>
2022-03-24 16:35:26 +0800

    Create GsFinPreviewSdk.php
    财务鹰眼

---
+++ src/Exports/Modules/GsFinPreviewSdk.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * 重要说明
+ * 1. 本文件由Postman命令脚本自动生成, 请不要修改, 若需修改
+ *    请通过`php console postman`命令重新生成.
+ * 2. 本脚本在生成时, 依赖所在项目的Controller有 `@Sdk method`定义,
+ *    同时, 项目根目录下的`postman.json`需有`sdk`、`sdkLink`定义
+ * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
+ *    的`src/Exports/Modules`目录下，并发重新发布release版本.
+ * @author PostmanCommand
+ * @date   2022-01-12
+ * @time   Thu, 02 Apr 2020 01:00:11 +0800
+ */
+namespace Uniondrug\ServiceSdk\Exports\Modules;
+
+use Uniondrug\ServiceSdk\Exports\Abstracts\SdkBase;
+use Uniondrug\ServiceSdk\Bases\ResponseInterface;
+
+/**
+ * Class GsFinPreviewSdk
+ * @package Uniondrug\ServiceSdk\Exports\Modules
+ */
+class GsFinPreviewSdk extends SdkBase
+{
+    /**
+     * 订单详情
+     * @param $body
+     * @return ResponseInterface
+     */
+    public function orderDetail($body)
+    {
+        return $this->restful("GET", "/order/detail", $body);
+    }
+}
