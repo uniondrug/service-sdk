fad74d6a74f0767ffaa4517f802e02d7d55f38a8
weng <shushun@uniondrug.cn>
2022-04-11 14:14:19 +0800

    增加 一体化平台，会员

--- src/Exports/Modules/MerchantSdk.php
+++ src/Exports/Modules/MerchantSdk.php
@@ -8,8 +8,8 @@
  * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
- * @date   2022-03-30
- * @time   Wed, 30 Mar 2022 13:59:47 +0800
+ * @date   2022-04-11
+ * @time   Mon, 11 Apr 2022 14:13:30 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
@@ -365,6 +365,32 @@ class MerchantSdk extends SdkBase
     public function backEndEditIsGroup($body, $query = null, $extra = null)
     {
         return $this->restful("POST", "/organizebasebackend/editisgroup", $body, $query, $extra);
+    }
+
+    /**
+     * 修改会员
+     * @link https://uniondrug.coding.net/p/module.merchant/git/blob/development/docs/api/OrganizeBaseBackendController/editIsMemberAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function backEndEditIsMember($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/organizebasebackend/editismember", $body, $query, $extra);
+    }
+
+    /**
+     * 修改一体化平台
+     * @link https://uniondrug.coding.net/p/module.merchant/git/blob/development/docs/api/OrganizeBaseBackendController/editOnePlatformAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function backEndEditOnePlatform($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/organizebasebackend/editoneplatform", $body, $query, $extra);
     }
 
     /**
