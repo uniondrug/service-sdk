633de77021ec69f2298515d769af9cb06a20c384
weng <shushun@uniondrug.cn>
2022-05-07 11:47:09 +0800

    增加 一体化平台，会员

--- src/Exports/Modules/MerchantSdk.php
+++ src/Exports/Modules/MerchantSdk.php
@@ -8,8 +8,8 @@
  * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
  *    的`src/Exports/Modules`目录下，并发重新发布release版本.
  * @author PostmanCommand
- * @date   2022-04-11
- * @time   Mon, 11 Apr 2022 14:13:30 +0800
+ * @date   2022-05-07
+ * @time   Sat, 07 May 2022 11:46:27 +0800
  */
 namespace Uniondrug\ServiceSdk\Exports\Modules;
 
@@ -352,6 +352,19 @@ class MerchantSdk extends SdkBase
     public function assistantsSumMember($body, $query = null, $extra = null)
     {
         return $this->restful("POST", "/assistants/summember", $body, $query, $extra);
+    }
+
+    /**
+     * 修改
+     * @link https://uniondrug.coding.net/p/module.merchant/git/blob/development/docs/api/OrganizeBaseBackendController/editAppletServiceAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function backEndEditAppletService($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/organizebasebackend/editappletservice", $body, $query, $extra);
     }
 
     /**
