006de58a288aeaba8730204045d83085a2af9570
康小强 <kangxiaoqiang@hoge.cn>
2022-05-17 16:19:24 +0800

    resource.module

--- src/Exports/Modules/YdbResPmSdk.php
+++ src/Exports/Modules/YdbResPmSdk.php
@@ -286,4 +286,31 @@ class YdbResPmSdk extends SdkBase
     {
         return $this->restful("POST", "/group/statusUpdate", $body, $query, $extra);
     }
+
+    /**
+     * 绑定资源
+     * @link https://uniondrug.coding.net/p/module.pm.res.ydb/git/blob/development/docs/api/GroupController/bindResourceAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function resourceGroupBindResource($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/group/bindResource", $body, $query, $extra);
+    }
+
+    /**
+     * 取消绑定资源
+     * @link https://uniondrug.coding.net/p/module.pm.res.ydb/git/blob/development/docs/api/GroupController/cancelBindResourceAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function resourceGroupCancelBindResource($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/group/cancelBindResource", $body, $query, $extra);
+    }
+
 }
