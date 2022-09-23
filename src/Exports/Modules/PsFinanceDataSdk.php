1d5b44761c61b605b31c25351dec3549fec2e0a5
liu.haifeng <liuhaifeng@uniondrug.com>
2022-09-23 14:13:12 +0800

    公共数据sdk

--- src/Exports/Modules/PsFinanceDataSdk.php
+++ src/Exports/Modules/PsFinanceDataSdk.php
@@ -2964,4 +2964,17 @@ class PsFinanceDataSdk extends SdkBase
     {
         return $this->restful("POST", "/tax/classification/rule/update", $body, $query, $extra);
     }
+
+    /**
+     * 检查组织下银行账号是否开通银企直联
+     * @link http://torna.uniondrug.cn/#/view/32QmJPB2
+     * @param array|object $body 入参类型
+     * @param null $query Query数据
+     * @param null $extra 请求头信息
+     * @return ResponseInterface
+     */
+    public function bankAccountCheckOnline($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/bank/account/check/online", $body, $query, $extra);
+    }
 }
