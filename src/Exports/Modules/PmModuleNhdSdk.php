87ca0756cdfed8131a6ee3ca05d7cfb48752dc6e
weng.pengshun <shushun@uniondrug.com>
2022-12-07 15:52:54 +0800

    增加nhd module

---
+++ src/Exports/Modules/PmModuleNhdSdk.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * 重要说明
+ * 1. 本文件由Postman命令脚本自动生成, 请不要修改, 若需修改
+ *    请通过`php console postman`命令重新生成.
+ * 2. 本脚本在生成时, 依赖所在项目的Controller有 `@Sdk method`定义,
+ *    同时, 项目根目录下的`postman.json`需有`sdk`、`sdkLink`定义
+ * 3. 发布SDK，请将本文件放到`uniondrug/service-sdk`项目
+ *    的`src/Exports/Backends`目录下，并发重新发布release版本.
+ * @author PostmanCommand
+ * @date   2022-12-07
+ * @time   Wed, 07 Dec 2022 15:50:29 +0800
+ */
+namespace Uniondrug\ServiceSdk\Exports\Modules;
+
+
+use Uniondrug\ServiceSdk\Exports\Abstracts\SdkBase;
+use Uniondrug\ServiceSdk\Bases\ResponseInterface;
+
+/**
+ * PmModuleNhdSdk
+ * @package Uniondrug\ServiceSdk\Modules
+ */
+class PmModuleNhdSdk extends SdkBase
+{
+    /**
+     * 服务名称
+     * 自来`postman.json`文件定义的`sdkService`值
+     * @var string
+     */
+    protected $serviceName = 'pm-module-nhd';
+
+    /**
+     * paging
+     * @link https://uniondrug.coding.net/p/pm-module-nhd/git/blob/development/docs/api/InspectController/pagingAction.md
+     * @param array|object $body 入参类型
+     * @param null $query  Query数据
+     * @param null $extra  请求头信息
+     * @return ResponseInterface
+     */
+    public function inspectPaging($body, $query = null, $extra = null)
+    {
+        return $this->restful("POST", "/inspect/paging", $body, $query, $extra);
+    }
+}
