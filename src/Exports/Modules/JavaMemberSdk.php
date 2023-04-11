c1669273343970be6b5d06088b947b49cae375c5
张浩 <zhanghao@zhanghaodeMacBook-Pro.local>
2023-04-11 15:49:54 +0800

    javaMember添加userInfoAddOr方法

--- src/Exports/Modules/JavaMemberSdk.php
+++ src/Exports/Modules/JavaMemberSdk.php
@@ -524,4 +524,15 @@ class JavaMemberSdk extends SdkBase
     {
         return $this->restful("POST", "/v2/api/userBasic/batchByAccounts", $body);
     }
+
+    /**
+     * 添加用户已存在则查询用户
+     * @link https://uniondrug.coding.net/p/java.middlend.usercenter/d/java.middlend.usercenter/git/tree/feature_api_v1.0.2/api-docs/uniondrug.usercenter.provider/UserBasicApiController/addOrQueryApi.md
+     * @param array|object $body 入参类型
+     * @return ResponseInterface
+     */
+    public function userInfoAddOr($body)
+    {
+        return $this->restful("POST", "/v2/api/userBasic/addOrQuery", $body);
+    }
 }
